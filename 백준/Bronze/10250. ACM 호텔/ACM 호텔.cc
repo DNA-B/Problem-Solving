4d7992cdfd09dd940562068a58c800184c88993b
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-22 19:22:01 +0900

    [Bronze II] Title: ACM 호텔, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10250. ACM 호텔/ACM 호텔.cc
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() 
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int h, w, n;
+    int res;
+    int rep;
+
+    cin >> rep;
+
+    while (rep--)
+    {
+        cin >> h >> w >> n;
+
+        if (n % h == 0) {
+            res = h * 100 + (n / h);
+        }
+        else 
+        {
+            res = (n % h) * 100 + (n / h) + 1;
+        }
+        cout << res << "\n";
+    }
+   
+    return 0;
+}
