172c2065aa5832be6b404307b5c9682e831010db
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-22 19:14:42 +0900

    [Bronze V] Title: A-B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1001. A－B/A－B.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <string>
+
+using namespace std;
+
+int main() 
+{
+	int a = 0;
+	int b = 0;
+
+	cin >> a;
+	cin >> b;
+
+	cout << a - b;
+
+	
+	return 0;
+
+}
