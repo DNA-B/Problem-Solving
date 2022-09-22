34547e43668e0e546f5b708ab711be891bd1523d
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-22 19:39:28 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <cctype>
+#include <cstring>
+#include <cstdlib>
+#include <cmath>
+#include <algorithm>
+#include <string>
+#include <queue>
+
+using namespace std;
+
+//
+int main()
+{
+	int n;
+	int m;
+	int max = -1;
+	int arr[100] = { 0 };
+
+	cin >> n >> m;
+
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+
+	for (int i = 0; i < n - 2; i++)
+	{
+		for (int j = i+1; j < n - 1; j++)
+		{
+			for (int k = j + 1; k < n; k++)
+			{
+				if (arr[i] + arr[j] + arr[k] <= m && arr[i] + arr[j] + arr[k] >= max)
+				{
+					max = arr[i] + arr[j] + arr[k];
+				}
+			}
+		}
+	}
+
+	cout << max;
+
+	return 0;
+}
