f336054aaa49e36c5535149e3b66702acbe12379
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-22 19:19:54 +0900

    [Silver V] Title: 수 정렬하기 5, Time: 4768 ms, Memory: 8292 KB -BaekjoonHub

---
+++ 백준/Silver/15688. 수 정렬하기 5/수 정렬하기 5.cc
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    vector<int> v;
+    int rep;
+    int x;
+
+    cin >> rep;
+
+    while (rep--)
+    {
+        cin >> x;
+        v.push_back(x);
+    }
+
+    sort(v.begin(), v.end());
+
+    for (int item : v)
+    {
+        cout << item << "\n";
+    }
+
+    return 0;
+}
