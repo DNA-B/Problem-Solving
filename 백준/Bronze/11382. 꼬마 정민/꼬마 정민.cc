d03c6bed30c350e0c0d516b2d4c193fb96f14464
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-24 11:29:56 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <algorithm> 
+// #include <string> 
+// #include <cctype> 
+// #include <cmath> 
+// #include <queue>
+// #include<deque>
+// #include <set>
+ #include <vector>
+// #include <stack> 
+// #include <bitset>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+    long long a, b, c;
+
+	cin >> a >> b >> c;
+
+	cout << a + b + c;
+
+	return 0;
+}
