224f88386376843337f84e7764cd06d957df542b
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-26 02:10:51 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> seoul) {
+    string answer = "";
+    int idx = 0;
+    
+    for(int i = 0;i<seoul.size();i++)
+    {
+        if(seoul[i]=="Kim")
+        {
+            idx = i;
+            break;
+        }
+    }
+    
+    answer += "김서방은 ";
+    answer += to_string(idx);
+    answer += "에 있다";
+    
+    return answer;
+}
