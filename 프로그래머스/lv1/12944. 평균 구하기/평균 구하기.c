afd37979edf48493c7ef172c9633540e2ed5b5cd
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-26 02:03:48 +0900

    [level 1] Title: 평균 구하기, Time: 0.01 ms, Memory: 4.17 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12944. 평균 구하기/평균 구하기.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+// arr_len은 배열 arr의 길이입니다.
+double solution(int arr[], size_t arr_len) {
+    double answer = 0;
+    int sum = 0;
+    
+    for(int i=0;i<arr_len;i++)
+    {
+        sum += arr[i];
+    }
+    
+    answer = sum/(double)arr_len;
+    
+    return answer;
+}
