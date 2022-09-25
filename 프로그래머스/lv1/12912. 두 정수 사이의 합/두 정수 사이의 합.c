c30f72eba17371276ad98149b50f38c05fe31dfa
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-26 02:07:49 +0900

    [level 1] Title: 두 정수 사이의 합, Time: 0.01 ms, Memory: 3.68 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12912. 두 정수 사이의 합/두 정수 사이의 합.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    
+    if( a == b)
+    {
+        return a;
+    }
+    
+    if( a > b)
+    {
+        for (int i = b; i <= a; i++) {
+         answer += i;
+      }
+    }
+    else
+    {
+        for (int i = a; i <= b; i++) {
+         answer += i;
+      }
+    }
+    
+    return answer;
+}
