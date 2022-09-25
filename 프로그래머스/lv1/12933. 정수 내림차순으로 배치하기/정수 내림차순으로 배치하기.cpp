1b202f33d371b5a85120a4f04b9046c8f7a6cd0e
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-25 22:03:24 +0900

    [level 1] Title: 정수 내림차순으로 배치하기, Time: 0.02 ms, Memory: 4.09 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12933. 정수 내림차순으로 배치하기/정수 내림차순으로 배치하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+long long solution(long long n) {
+    long long answer = 0;
+    
+    string str = to_string(n);
+    sort(str.begin(), str.end(), greater<>());
+    
+    answer = stoll(str);
+    
+    return answer;
+}
