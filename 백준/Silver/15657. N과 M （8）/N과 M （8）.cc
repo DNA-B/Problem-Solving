dfae8d0573ac5eaae64a0d8935a8059634498a42
DNA <102334596+DNA-B@users.noreply.github.com>
2022-09-26 13:56:04 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int n, m;
+int arr[8];
+int num[8];
+bool isused[8];
+
+
+void func(int k) { // 현재 k개까지 수를 택했음.
+    if (k == m) 
+    { // m개를 모두 택했으면
+        for (int i = 0; i < m; i++)
+            cout << arr[i] << ' '; // arr에 기록해둔 수를 출력
+        cout << '\n';
+        return;
+    }
+
+    for (int i = 0; i < n; i++)
+    { // 1부터 n까지의 수에 대해
+        if (k == 0)
+        {
+            arr[k] = num[i]; // k번째 수를 i로 정함
+            func(k + 1); // 다음 수를 정하러 한 단계 더 들어감
+
+        }
+        else if (k > 0 && arr[k - 1] <= num[i])
+        {
+            arr[k] = num[i]; // k번째 수를 i로 정함
+            func(k + 1); // 다음 수를 정하러 한 단계 더 들어감
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; ++i)
+    {
+        cin >> num[i];
+    }
+        
+    sort(num, num + n);
+
+    func(0);
+
+    return 0;
+}
