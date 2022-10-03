33aac65020cfc076a4a9dabbf29aeadd80112cf9
DNA <102334596+DNA-B@users.noreply.github.com>
2022-10-04 05:36:21 +0900

    [Silver II] Title: N과 M (9), Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.cc
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int n, m;
+int arr[10];
+int num[10];
+bool isused[10];
+
+void func(int k) 
+{ // 현재 k개까지 수를 택했음.
+    if (k == m) 
+    {
+        for (int i = 0; i < m; ++i)
+        {
+            cout << arr[i] << ' ';
+        }
+        cout << '\n';
+        return;
+    }
+
+    int tmp = 0;  // 중복 수열 확인 변수
+
+    for (int i = 0; i < n; ++i)
+    {
+        if (!isused[i] && tmp != num[i]) 
+        { // 이전 수열의 마지막 항과 새로운 수열의 마지막 항이 같으면 중복 수열
+            isused[i] = true;
+            arr[k] = num[i];
+            tmp = arr[k];
+            func(k + 1);
+            isused[i] = false;
+        }
+    }
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; ++i)
+        cin >> num[i];
+
+    sort(num, num + n);
+    func(0);
+
+    return 0;
+}
