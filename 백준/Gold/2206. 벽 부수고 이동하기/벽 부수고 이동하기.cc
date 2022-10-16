a19c9889a8e3cdaf95be6a6c60ad571f9409e3cd
DNA <102334596+DNA-B@users.noreply.github.com>
2022-10-16 20:04:43 +0900

    [Gold IV] Title: 벽 부수고 이동하기, Time: 92 ms, Memory: 13300 KB -BaekjoonHub

---
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+string board[1000];
+int vis[1000][1000][2]; // x, y, 벽 부쉈는지 여부
+
+int n, m;
+int dx[4] = { -1, 1, 0, 0 }; // 상하좌우
+int dy[4] = { 0, 0, -1 ,1 };
+
+int bfs()
+{
+    queue<pair<pair<int, int>, pair<int, int>>> q;
+
+    q.push({ { 0,0 }, {0,1} }); // {x,y}, {벽 부순 여부, 거리}
+    vis[0][0][0] = 1;
+
+    while (!q.empty())
+    {
+        auto cur = q.front();
+        int is_crash = cur.second.first;
+        int cnt = cur.second.second;
+
+        q.pop();
+
+        if ((cur.first.first == n - 1) && (cur.first.second == m - 1))
+            return cnt; // 도착
+
+        for (int dir = 0; dir < 4; dir++)
+        {
+            int nx = cur.first.first + dx[dir]; // 방향 설정
+            int ny = cur.first.second + dy[dir];
+
+            if (nx < 0 || nx >= n || ny >= m || ny < 0)
+                continue; // 범위 밖일 경우
+
+            if (board[nx][ny] == '1' && (cur.second.first) == 0)
+            {
+                vis[nx][ny][is_crash + 1] = 1;
+                q.push({ {nx,ny},{is_crash + 1, cnt + 1} });
+            }
+            else if (board[nx][ny] == '0' && vis[nx][ny][is_crash] == 0)
+            {
+                vis[nx][ny][is_crash] = 1;
+                q.push({ { nx,ny }, {is_crash, cnt + 1} });
+            }
+        }
+    }
+    // 이 라인까지 왔으면 목적지에 도착하지 못한 것이므로 -1 반환
+    return -1;
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> n >> m;
+
+    for (int i = 0; i < n; i++)
+    {
+        cin >> board[i];
+    }
+
+    cout << bfs();
+
+    return 0;
+}
