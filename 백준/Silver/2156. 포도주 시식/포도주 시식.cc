10f03bad4503f929fc189950f177b41a9a66efe8
DNA <102334596+DNA-B@users.noreply.github.com>
2023-01-04 16:08:37 +0900

    [Silver I] Title: 포도주 시식, Time: 0 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.cc
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.cc
@@ -2,6 +2,12 @@
 
 using namespace std;
 
+/* BOJ 2156 : 포도주 시식
+- 계단 오르기와 비슷한 풀이
+- 그러나 목적지에 도착하는 것이 목표가 아니기 때문에
+- 마지막 잔을 무조건 마실 필요가 없다는 것이 주의점.
+*/
+
 int dp[10005];
 vector<int> v(10005);
 
@@ -19,26 +25,14 @@ int main()
         cin >> v[i];
     }
 
-   
+    dp[1] = v[1];
+    dp[2] = v[1] + v[2];
 
-    for (int j = 1; j <= n; j++)
-    {
-        fill(dp, dp + 10005, 0);
-        dp[j] = v[j];
-        dp[j + 1] = v[j] + v[j + 1];
-
-        for (int i = 3; i <= n; i++)
-            dp[i] = max({dp[i - 2] + v[i], dp[i - 3] + v[i - 1] + v[i], dp[i - 1]
-    });
-
-        for (int i = 1; i <= n; i++)
-        {
-            if (dp[i] > res)
-                res = dp[i];
-        }
-    }
+    for (int i = 3; i <= n; i++)
+        dp[i] = max({ dp[i - 2] + v[i], dp[i - 3] + v[i - 1] + v[i], dp[i - 1]
+            });
      
-    cout << res;
+    cout << dp[n];
 
     return 0;
 }
