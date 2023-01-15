b020b47213ac03400a7e4e149df7a654634f52b9
DNA <102334596+DNA-B@users.noreply.github.com>
2023-01-15 19:10:57 +0900

    [Bronze IV] Title: 알고리즘 수업 - 알고리즘의 수행 시간 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24263. 알고리즘 수업 － 알고리즘의 수행 시간 2/알고리즘 수업 － 알고리즘의 수행 시간 2.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n = 0;
+
+    cin >> n;
+
+    cout << n << "\n" << 1;
+    return 0;
+}
