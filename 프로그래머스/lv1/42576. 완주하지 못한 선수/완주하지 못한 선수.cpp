6da0f0d0cc6e3a6938199c32f607ad6c3b9970f3
DNA <102334596+DNA-B@users.noreply.github.com>
2023-01-16 21:04:44 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 122.58 ms, Memory: 32.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42576. 완주하지 못한 선수/완주하지 못한 선수.cpp
@@ -0,0 +1,25 @@
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+
+string solution(vector<string> participant, vector<string> completion) {
+    string answer = "";
+    map<string, int> is_completion;
+    
+    for(int i = 0;i<completion.size();i++)
+        is_completion[completion[i]]++;
+    
+    for(int i = 0;i<participant.size();i++)
+    {
+        if(is_completion[participant[i]] > 0)
+            is_completion[participant[i]]--;
+        else
+            answer += participant[i];
+        
+    }
+    
+    return answer;
+}
