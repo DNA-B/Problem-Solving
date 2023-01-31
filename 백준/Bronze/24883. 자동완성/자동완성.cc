a09a39b272d2ca4b8da4a596d4948e6dd616bc4e
DNA-B <102334596+DNA-B@users.noreply.github.com>
2023-01-31 20:27:36 +0900

    [Bronze IV] Title: 자동완성, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24883. 자동완성/자동완성.cc
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+   
+    char c;
+
+    cin >> c;
+
+    if (c == 'N' || c == 'n')
+        cout << "Naver D2";
+    else
+        cout << "Naver Whale";
+
+    return 0;
+}
