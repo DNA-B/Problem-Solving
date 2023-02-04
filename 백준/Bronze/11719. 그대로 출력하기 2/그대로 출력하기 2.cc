5d73ed19cf61f6568b83a8ea485ed82a9f41b601
DNA-B <102334596+DNA-B@users.noreply.github.com>
2023-02-04 17:58:47 +0900

    [Bronze III] Title: 그대로 출력하기 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+ 
+    string str;
+
+    while (getline(cin, str))
+        cout << str << '\n';
+
+    return 0;
+}
