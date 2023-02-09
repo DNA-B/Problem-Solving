d1691609b7bdc1a18a1c917323b453a98b0d565c
DNA-B <102334596+DNA-B@users.noreply.github.com>
2023-02-09 21:53:17 +0900

    [Bronze II] Title: 16진수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/1550. 16진수/16진수.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int hex_num;
+
+    cin >> hex >> hex_num;
+    cout << hex_num;
+
+    return 0;
+}
