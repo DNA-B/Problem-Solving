29658dd36a3124b1489cc022d43f1d3ae9b0a100
DNA-B <102334596+DNA-B@users.noreply.github.com>
2023-02-17 17:09:49 +0900

    [Bronze II] Title: 피보나치 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.cc
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int mem[46];
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int n;
+
+    cin >> n;
+
+    mem[0] = 0;
+    mem[1] = 1;
+
+    for (int i = 2; i <= n; i++)
+        mem[i] = mem[i - 2] + mem[i - 1];
+
+    cout << mem[n];
+
+    return 0;
+}
