9851eae056d175fc8661b64b697b6b9c48f8f1e5
변성훈_T5100 <102334596+DNA-B@users.noreply.github.com>
2023-03-16 19:04:51 +0900

    [Silver I] Title: 절댓값 힙, Time: 16 ms, Memory: 2532 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.cc
@@ -2,39 +2,46 @@
 
 using namespace std;
 
+int N, x;
+
+struct cmp 
+{
+    bool operator()(int x, int y)
+    {
+        if (abs(x) == abs(y)) // 같으면 실제로 작은 값
+            return x > y;
+        else
+            return abs(x) > abs(y);     
+    }
+};
+
 int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    // 절댓값 기준 오름차순 정렬 하기 위해 greater, 또한 원래 값을 보존하기 위해 pair사용
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    int rep;
-    long long n;
+    priority_queue<int, vector<int>, cmp> pq;
 
-    cin >> rep;
-
-    while (rep--)
+    cin >> N;
+    
+    while (N--)
     {
-        cin >> n;
-     
-        if (n == 0)
+        cin >> x;
+
+        if (x == 0)
         {
             if (pq.empty())
             {
-                cout << 0 << '\n';
+                cout << 0 << "\n";
                 continue;
             }
-            else
-            {
-                cout << pq.top().second << '\n';
-                pq.pop();
-            }
+
+            cout << pq.top() << "\n";
+            pq.pop();
+            continue;
         }
-        else
-        {
-            pq.push({ abs(n), n });
-        }
+
+        pq.push(x);  
     }
 
     return 0;
