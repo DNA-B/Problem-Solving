20910e01ee68fb504c2e622a4e003791d2c2dbcc
변성훈_T5100 <102334596+DNA-B@users.noreply.github.com>
2023-03-29 23:04:46 +0900

    [Gold V] Title: 용액, Time: 12 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.cc
+++ 백준/Gold/2467. 용액/용액.cc
@@ -20,16 +20,19 @@ int main()
     p1 = 0;
     p2 = N - 1;
 
-    while (p1 < p2)
+    while (p1 < p2) // 0에 가까운 값?
     {
-        if (abs(arr[p1] + arr[p2]) < v_min)
+        if (abs(arr[p1] + arr[p2]) < v_min) // abs를 안 씌우면 min이 (-무한대)에 가까워진다.
         {
             idx1 = p1;
             idx2 = p2;
             v_min = abs(arr[p1] + arr[p2]);
         }
-
-        if (abs(arr[p1]) > abs(arr[p2]))
+        
+        // 포인터를 어떻게 움직일 것인가?
+        // 이미 정렬되어 있는 배열이 입력으로 주어지므로
+        // 절대값이 더 큰 곳이 움직이면 두 값의 gap은 줄어들 것이다.
+        if (abs(arr[p1]) > abs(arr[p2])) 
             p1++;
         else
             p2--;
