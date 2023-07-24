7d2a82989b78789672c5a42becf8ac69e004a231
변성훈_T5100 <sphz3813@gmail.com>
2023-07-24 14:31:21 +0900

    [Silver III] Title: 안테나, Time: 28 ms, Memory: 2800 KB -BaekjoonHub

--- 백준/Silver/18310. 안테나/안테나.cc
+++ 백준/Silver/18310. 안테나/안테나.cc
@@ -5,7 +5,6 @@ using namespace std;
 
 int N;
 int arr[200001];
-int ans = 0;
 
 int main(void)
 {
@@ -14,25 +13,11 @@ int main(void)
 
     cin >> N;
 
-    double mid_value = 0;
     for (int i = 0; i < N; i++)
         cin >> arr[i];
 
     sort(arr, arr + N);
-
-    // 중앙값 계산
-    if (N % 2 == 0)
-        mid_value = (arr[N / 2 - 1] + arr[N / 2]) / 2;
-    else
-        mid_value = arr[N / 2];
-
-    for (int i = 0; i < N; i++)
-    {
-        if (arr[i] <= mid_value) // 중앙값보다 커지는 순간이 오기 전에 break
-            ans = arr[i];
-    }
-
-    cout << ans;
+    cout << arr[(N - 1) / 2];
 
     return 0;
 }
