a12679d130101f4cd6c87a90f4e1bcc42c3bf7d5
변성훈_T5100 <sphz3813@gmail.com>
2023-07-26 16:30:04 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 1624 ms, Memory: 49016 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.cc
@@ -4,9 +4,7 @@
 using namespace std;
 
 int T, N;
-priority_queue<int> max_q;
-priority_queue<int, vector<int>, greater<>> min_q;
-map<int, int> check;
+multiset<int> ms;
 
 int main(void)
 {
@@ -18,62 +16,37 @@ int main(void)
     while (T--)
     {
         // clear
-        max_q = {};
-        min_q = {};
-        check = {};
+        ms = {};
 
         cin >> N;
 
         char order;
-        for (int i = 0; i < N; i++)
+        int x;
+        while (N--)
         {
-            cin >> order;
+            cin >> order >> x;
 
             if (order == 'I') // 삽입
             {
-                int x;
-
-                cin >> x;
-
-                max_q.emplace(x);
-                min_q.emplace(x);
-                check[x]++;
+                ms.emplace(x);
             }
             else if (order == 'D') // 삭제
             {
-                int max_or_min;
+                if (ms.empty())
+                    continue;
 
-                cin >> max_or_min;
-
-                if (max_or_min == 1) // 최댓값 삭제
-                {
-                    if (!max_q.empty())
-                    {
-                        check[max_q.top()]--;
-                        max_q.pop();
-                    }
-                }
-                else if (max_or_min == -1) // 최솟값 삭제
-                {
-                    if (!min_q.empty())
-                    {
-                        check[min_q.top()]--;
-                        min_q.pop();
-                    }
-                }
-
-                while (!max_q.empty() && check[max_q.top()] < 1) // 최소에서 삭제된 값 비우기
-                    max_q.pop();
-
-                while (!min_q.empty() && check[min_q.top()] < 1) // 최대에서 삭제된 값 비우기
-                    min_q.pop();
+                if (x == 1) // 최댓값 삭제
+                    ms.erase(--ms.end());
+                else if (x == -1) // 최솟값 삭제
+                    ms.erase(ms.begin());
             }
         }
 
-        if (max_q.empty() || min_q.empty())
+        if (ms.empty())
             cout << "EMPTY\n";
         else
-            cout << max_q.top() << " " << min_q.top() << "\n";
+            cout << *(--ms.end()) << " " << *ms.begin() << "\n";
     }
+
     return 0;
 }
