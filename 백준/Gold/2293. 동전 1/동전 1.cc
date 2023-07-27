df407477fe2789fb7d556823f33af12a1c12dfcd
변성훈_T5100 <sphz3813@gmail.com>
2023-07-27 11:22:13 +0900

    [Gold V] Title: 동전 1, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Gold/2293. 동전 1/동전 1.cc
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define INF 0x3f3f3f3f
+
+using namespace std;
+
+int N, K;
+int mem[10001];
+int arr[101];
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> K;
+
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
+
+    mem[0] = 1;
+    for (int i = 0; i < N; i++)
+        for (int j = arr[i]; j <= K; j++)
+            mem[j] += mem[j - arr[i]];
+
+    cout << mem[K] << endl;
+
+    return 0;
+}
