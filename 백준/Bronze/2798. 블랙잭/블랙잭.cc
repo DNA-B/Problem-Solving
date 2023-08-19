fac1b9ffc63c1b39d2a7d91b9d200e8006ab4e11
변성훈 <sphz3813@gmail.com>
2023-08-19 22:38:34 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -9,7 +9,6 @@
 
 using namespace std;
 
-//
 int main()
 {
 	int n;
@@ -19,25 +18,14 @@ int main()
 
 	cin >> n >> m;
 
-
 	for (int i = 0; i < n; i++)
-	{
 		cin >> arr[i];
-	}
 
 	for (int i = 0; i < n - 2; i++)
-	{
-		for (int j = i+1; j < n - 1; j++)
-		{
+		for (int j = i + 1; j < n - 1; j++)
 			for (int k = j + 1; k < n; k++)
-			{
 				if (arr[i] + arr[j] + arr[k] <= m && arr[i] + arr[j] + arr[k] >= max)
-				{
 					max = arr[i] + arr[j] + arr[k];
-				}
-			}
-		}
-	}
 
 	cout << max;
 
