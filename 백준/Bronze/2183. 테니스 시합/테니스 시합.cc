6bdcfa1932adabb4c070dad2ba4ec205490da7b2
변성훈 <sphz3813@gmail.com>
2023-08-24 23:39:21 +0900

    [Bronze I] Title: 테니스 시합, Time: 0 ms, Memory: 2168 KB -BaekjoonHub

---
+++ 백준/Bronze/2183. 테니스 시합/테니스 시합.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define INF 0x3f3f3f3f
+#define IDX 1000
+using namespace std;
+
+int N;
+string s;
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N >> s;
+    cout << s[s.length() - 1];
+
+    return 0;
+}
