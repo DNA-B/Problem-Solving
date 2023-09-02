21a2002eeea6b54c40df2ee86a87868991becca8
변성훈 <sphz3813@gmail.com>
2023-09-02 16:55:49 +0900

    [Bronze I] Title: 초콜릿 자르기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2163. 초콜릿 자르기/초콜릿 자르기.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+#define INF 0x3f3f3f3f
+
+using namespace std;
+
+
+int main(void)
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int N, M;
+
+    cin >> N >> M;
+    cout << (N * M) - 1;
+
+    return 0;
+}
