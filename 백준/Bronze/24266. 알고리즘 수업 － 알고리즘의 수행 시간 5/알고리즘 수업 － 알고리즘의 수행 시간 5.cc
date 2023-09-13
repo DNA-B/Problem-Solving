04e4fba8fc09f5907c862620e9caaf3977b0147a
변성훈 <sphz3813@gmail.com>
2023-09-13 23:46:53 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define INF 0x3f3f3f3f
+
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    long long n;
+    
+    cin >> n;
+    cout << n * n * n << '\n' << 3;
+
+    return 0;
+}
