f36d5bde56d76a166e08e1b12681accf227da96d
변성훈 <sphz3813@gmail.com>
2023-10-10 05:25:40 +0900

    [Bronze V] Title: 오늘 날짜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
+++ 백준/Bronze/10699. 오늘 날짜/오늘 날짜.cc
@@ -1,23 +1,10 @@
 #include <iostream>
-#include <algorithm> 
-// #include <string> 
-// #include <cctype> 
-// #include <cmath> 
-// #include <queue>
-// #include<deque>
-// #include <set>
- #include <vector>
-// #include <stack> 
-// #include <bitset>
 
 using namespace std;
 
-int main()
+int main() 
 {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-	
-	cout << "2022-08-13";
-
+    cout<<"2023-10-10";
+    
 	return 0;
 }
