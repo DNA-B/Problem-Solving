dda79edaf059b3c36668b8ae3f500c57c0c4c4d2
변성훈 <sphz3813@gmail.com>
2023-11-17 05:11:20 +0900

    [Silver II] Title: 가장 긴 감소하는 부분 수열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
+++ 백준/Silver/11722. 가장 긴 감소하는 부분 수열/가장 긴 감소하는 부분 수열.cc
@@ -1,33 +1,32 @@
 #include <bits/stdc++.h>
 #define INF 0x3f3f3f3f
+#define MAX 100000000
 
 using namespace std;
 
-int arr[1001];
-int mem[1001];
 int N;
+int arr[1001];
+vector<int> mem;
 
-int main(void)
+int main()
 {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-	cin >> N;
+    cin >> N;
 
-	for (int i = 0; i < N; i++)
-		cin >> arr[i];
+    for (int i = 0; i < N; i++)
+        cin >> arr[i];
 
-	for (int i = 0; i < N; i++)
-	{
-		mem[i] = 1;
-		for (int j = 0; j < i; j++)
-		{
-			if (arr[i] < arr[j])
-				mem[i] = max(mem[i], mem[j] + 1);
-		}
-	}
+    for (int i = 0; i < N; i++)
+    {
+        if (mem.empty() || mem.back() > arr[i])
+            mem.emplace_back(arr[i]);
+        else
+            *lower_bound(mem.begin(), mem.end(), arr[i], greater<>()) = arr[i];
+    }
 
-	cout << *max_element(mem, mem + N);
+    cout << mem.size();
 
     return 0;
 }
