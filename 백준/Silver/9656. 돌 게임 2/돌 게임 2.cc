87151383ad7de7680537a5d56c1d38696d5027ae
변성훈 <sphz3813@gmail.com>
2023-12-31 19:40:02 +0900

    [Silver IV] Title: 돌 게임 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9656. 돌 게임 2/돌 게임 2.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> N;
+    cout << (N % 2 == 0 ? "SK" : "CY");
+
+    return 0;
+}
