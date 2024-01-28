0d0c86ec7e6529227d7e484b07774ff4a6705487
변성훈 <sphz3813@gmail.com>
2024-01-28 19:13:54 +0900

    [Bronze II] Title: 나머지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/3052. 나머지/나머지.cc
+++ 백준/Bronze/3052. 나머지/나머지.cc
@@ -1,31 +1,23 @@
-#include <iostream>
-#include <cctype>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <string>
+#include <bits/stdc++.h>
 
 using namespace std;
 
 int main()
 {
 	int count[42] = { 0 };
-	int arr[10] = { 0 };
+	int x;
 	int result = 0;
 
 	for (int i = 0; i < 10; i++)
 	{
-		cin >> arr[i];
-		count[arr[i] % 42]++;
+		cin >> x;
+		count[x % 42]++;
 	}
 
 	for (int i = 0; i < 42; i++)
 	{
 		if (count[i] != 0)
-		{
 			result++;
-		}
 	}
 
 	cout << result;
