0e99637a76f21f3e7049c4abca1c8247f6ab382d
변성훈 <sphz3813@gmail.com>
2024-02-02 20:44:46 +0900

    [Bronze I] Title: 창영이와 버스, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Bronze/22113. 창영이와 버스/창영이와 버스.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N, M;
+int A[101][101], chang[101];
+int price = 0;
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N >> M;
+
+	for (int i = 0; i < M; i++)
+		cin >> chang[i];
+
+	for (int i = 1; i <= N; i++)
+		for (int j = 1; j <= N; j++)
+			cin >> A[i][j];
+
+	for (int i = 1; i <= M; i++)
+		price += A[chang[i - 1]][chang[i]];
+
+	cout << price;
+
+	return 0;
+}
