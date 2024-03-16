d314c73f58c634d01da5a2e0e38b2f52c8e397de
변성훈 <sphz3813@gmail.com>
2024-03-16 16:24:13 +0900

    [Gold II] Title: 물대기, Time: 16 ms, Memory: 3196 KB -BaekjoonHub

---
+++ 백준/Gold/1368. 물대기/물대기.cc
@@ -0,0 +1,87 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int N;
+int n_edge = 0, cnt = 0, ans = 0;
+vector<int> p(301, -1);
+tuple<int, int, int> edge[100001];
+
+int uf_find(int x)
+{
+	if (p[x] < 0)
+		return x;
+
+	return p[x] = uf_find(p[x]);
+}
+
+bool uf_union(int a, int b)
+{
+	a = uf_find(a);
+	b = uf_find(b);
+
+	if (a == b)
+		return true;
+
+	if (p[a] == p[b])
+		p[a]--;
+
+	if (p[a] < p[b])
+		p[b] = a;
+	else
+		p[a] = b;
+
+	return false;
+}
+
+
+int main()
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N;
+
+	for (int cost, i = 1; i <= N; i++)
+	{
+		cin >> cost;
+		edge[n_edge++] = { cost, i, N + 1 };
+	}
+
+	for (int i = 1; i <= N; i++)
+	{
+		for (int cost, j = 1; j <= N; j++)
+		{
+			cin >> cost;
+
+			if (i >= j)
+				continue;
+
+			edge[n_edge++] = { cost, i, j };
+		}
+	}
+
+	N++; // 가상의 정점 생성
+	sort(edge, edge + n_edge);
+
+	for (int a, b, cost, i = 0; i < n_edge; i++)
+	{
+		tie(cost, a, b) = edge[i];
+
+		if (uf_union(a, b)) // 같은 그룹이면 continue;
+			continue;
+
+		ans += cost;
+		cnt++;
+
+		if (cnt == N - 1)
+			break;
+	}
+
+	cout << ans;
+
+	return 0;
+}
