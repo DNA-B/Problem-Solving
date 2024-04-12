0c020b9394c4f59a46a2dbea3240d53736aa0b65
변성훈 <sphz3813@gmail.com>
2024-04-12 23:16:44 +0900

    [Bronze V] Title: Rook, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cc
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	// freopen("input.txt", "r", stdin);
+
+	printf("  ___  ___  ___\n");
+	printf("  | |__| |__| |\n");
+	printf("  |           |\n");
+	printf("   \\_________/\n");
+	printf("    \\_______/\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |     |\n");
+	printf("     |_____|\n");
+	printf("  __/       \\__\n");
+	printf(" /             \\\n");
+	printf("/_______________\\\n");
+
+	return 0;
+}
