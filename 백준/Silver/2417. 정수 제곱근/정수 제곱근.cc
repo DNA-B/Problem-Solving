82dd89d24b2d4f6c7e54534476530583793a894e
변성훈 <sphz3813@gmail.com>
2024-04-20 07:55:41 +0900

    [Silver IV] Title: 정수 제곱근, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
+++ 백준/Silver/2417. 정수 제곱근/정수 제곱근.cc
@@ -10,6 +10,25 @@ using namespace std;
 
 ll N;
 
+ll binary_search(ll n) {
+	ll lo = -1, hi = LLONG_MAX;
+
+	while (lo + 1 < hi) {
+		// (lo + hi)하면서 long long 범위 넘어갈 수 있으므로 unsigned
+		unsigned ll mid = (lo + hi) / 2;
+
+		if ((mid * mid) == n)
+			return mid;
+
+		if ((mid * mid) < n)
+			lo = mid;
+		else
+			hi = mid;
+	}
+
+	return hi;
+}
+
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
@@ -17,13 +36,7 @@ int main() {
 	// freopen("input.txt", "r", stdin);
 
 	cin >> N;
-
-	ll sq_n = sqrt(N);
-
-	if ((sq_n * sq_n) < N)
-		sq_n++;
-
-	cout << sq_n;
+	cout << binary_search(N);
 
 	return 0;
 }
