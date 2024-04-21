d677424e5f4f8ce3f207dcb30d679f5567bda529
변성훈 <sphz3813@gmail.com>
2024-04-22 07:30:19 +0900

    [Silver III] Title: 게임, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1072. 게임/게임.cc
+++ 백준/Silver/1072. 게임/게임.cc
@@ -9,19 +9,23 @@
 using namespace std;
 
 ll x, y, z;
-int ans, win_rate;
 
-int check(ll x, ll y) { // 100 곱하면서 int 범위 넘어갈 수 있음
-	return 100 * y / x;
+bool check(int cur) { // 100 곱하면서 int 범위 넘어갈 수 있음
+	ll tmp = 100 * (y + cur) / (x + cur);
+
+	return tmp > z;
 }
 
-int binary_search(ll x, ll y, ll z) {
+int binary_search() {
 	int lo = 0, hi = BOUND;
+
+	if (!check(BOUND))
+		return -1;
 
 	while (lo + 1 < hi) {
 		int mid = (lo + hi) / 2;
 
-		if (check(x + mid, y + mid) > z)
+		if (check(mid))
 			hi = mid;
 		else
 			lo = mid;
@@ -37,15 +41,8 @@ int main() {
 	// freopen("input.txt", "r", stdin);
 
 	cin >> x >> y;
-
-	z = check(x, y);
-
-	if (z >= 99) { // 99%에서 변하려면 100%가 나와야하는데 100%는 모든 게임을 이겨야한다. 이미 패배가 있으므로 불가능하다.
-		cout << -1;
-		return 0;
-	}
-
-	cout << binary_search(x, y, z);
+	z = 100 * y / x;
+	cout << binary_search();
 
 	return 0;
 }
