78f7ae62462d255c799865893243ae88ab33dca0
변성훈 <sphz3813@gmail.com>
2024-05-09 20:48:14 +0900

    [Gold IV] Title: 녹색 옷 입은 애가 젤다지?, Time: 4 ms, Memory: 2216 KB -BaekjoonHub

--- 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
+++ 백준/Gold/4485. 녹색 옷 입은 애가 젤다지？/녹색 옷 입은 애가 젤다지？.cc
@@ -19,55 +19,57 @@ int dx[] = { 0, 0, -1, 1 };
 int dy[] = { 1, -1, 0, 0 };
 /***********************/
 
+int dijk() {
+	int d[BOUND][BOUND];
+	priority_queue <tuple<int, int, int>,
+		vector<tuple<int, int, int>>,
+		greater<tuple<int, int, int>>> pq;
+
+	fill(&d[0][0], &d[BOUND - 1][BOUND], INT_MAX);
+
+	for (int i = 0; i < N; i++)
+		for (int j = 0; j < N; j++)
+			cin >> board[i][j];
+
+	d[0][0] = board[0][0];
+	pq.push({ d[0][0], 0, 0 });
+
+	while (!pq.empty()) {
+		auto [cur_cost, cur_x, cur_y] = pq.top();
+		pq.pop();
+
+		if (d[cur_x][cur_y] != cur_cost)
+			continue;
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = cur_x + dx[dir];
+			int ny = cur_y + dy[dir];
+
+			if (nx < 0 or nx >= N or ny < 0 or ny >= N)
+				continue;
+			if (d[nx][ny] <= cur_cost + board[nx][ny])
+				continue;
+
+			d[nx][ny] = cur_cost + board[nx][ny];
+			pq.push({ d[nx][ny], nx, ny });
+		}
+	}
+
+	return d[N - 1][N - 1];
+}
+
 int main() {
 	cin.tie(0)->sync_with_stdio(0);
 
 	// freopen("input.txt", "r", stdin);
 
 	while (true) {
-		int d[BOUND][BOUND];
-		priority_queue <tuple<int, int, int>,
-			vector<tuple<int, int, int>>,
-			greater<tuple<int, int, int>>> pq;
-
-		fill(&d[0][0], &d[BOUND - 1][BOUND], INT_MAX);
-
 		cin >> N;
 
 		if (N == 0) // 입력 종료
 			break;
 
-		for (int i = 0; i < N; i++)
-			for (int j = 0; j < N; j++)
-				cin >> board[i][j];
-
-		d[0][0] = board[0][0];
-		pq.push({ d[0][0], 0, 0 });
-
-		while (!pq.empty()) {
-			int cur_cost, cur_x, cur_y;
-
-			tie(cur_cost, cur_x, cur_y) = pq.top();
-			pq.pop();
-
-			if (d[cur_x][cur_y] != cur_cost)
-				continue;
-
-			for (int dir = 0; dir < 4; dir++) {
-				int nx = cur_x + dx[dir];
-				int ny = cur_y + dy[dir];
-
-				if (nx < 0 || nx >= N || ny < 0 || ny >= N)
-					continue;
-				if (d[nx][ny] <= cur_cost + board[nx][ny])
-					continue;
-
-				d[nx][ny] = cur_cost + board[nx][ny];
-				pq.push({ d[nx][ny], nx, ny });
-			}
-		}
-
-		cout << "Problem " << problem++ << ": " << d[N - 1][N - 1] << "\n";
+		cout << "Problem " << problem++ << ": " << dijk() << "\n";
 	}
 
 	return 0;
