a20e66a8679e3071875c69cdd86be26fee013633
변성훈 <sphz3813@gmail.com>
2024-05-12 17:57:52 +0900

    [Gold IV] Title: 해킹, Time: 128 ms, Memory: 4184 KB -BaekjoonHub

--- 백준/Gold/10282. 해킹/해킹.cc
+++ 백준/Gold/10282. 해킹/해킹.cc
@@ -17,18 +17,15 @@ int T, N, D, C;
 /***********************/
 
 pair<int, int> dijk(vector<pair<int, int>> adj[BOUND], int st) {
-	int cnt, t = -1;
+	int cnt = 0, t = -1;
 	int d[BOUND];
-	int virus[BOUND]; // 감염 컴퓨터 체크 배열
 	priority_queue<pair<int, int>,
 		vector<pair<int, int>>,
 		greater<pair<int, int>>> pq;
 
 	fill(d, d + (N + 1), INT_MAX);
-	fill(virus, virus + (N + 1), 0);
 
 	d[st] = 0;
-	virus[st] = 1;
 	pq.push({ d[st], st });
 
 	while (!pq.empty()) {
@@ -38,16 +35,15 @@ pair<int, int> dijk(vector<pair<int, int>> adj[BOUND], int st) {
 		if (d[cur.Y] != cur.X)
 			continue;
 
+		cnt++; // 다익스트라는 각 정점을 한 번씩만 방문함
+
 		for (auto nxt : adj[cur.Y]) {
 			if (d[nxt.Y] > d[cur.Y] + nxt.X) {
 				d[nxt.Y] = d[cur.Y] + nxt.X;
-				virus[nxt.Y] = 1;
 				pq.push({ d[nxt.Y], nxt.Y });
 			}
 		}
 	}
-
-	cnt = count(virus, virus + (N + 1), 1); // 감염된 컴퓨터 개수 구하기
 
 	for (int i = 1; i <= N; i++) { // INT_MAX가 아닌 d 값중 최대값이 맨 마지막에 감염된 컴퓨터
 		if (d[i] == INT_MAX)
