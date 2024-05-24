58b7f37be6900e89d1f7b514709a0a6e4806f9d5
변성훈 <sphz3813@gmail.com>
2024-05-24 18:10:23 +0900

    [Bronze V] Title: Serca, Time: 96 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.cc
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+/******** MACRO ********/
+#define BOUND 200001
+#define MOD 1'000'000'007
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+using namespace std;
+
+/****** VARIABLEs ******/
+int N;
+/***********************/
+
+int main() {
+	cin.tie(nullptr)->sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N;
+
+	while (N--) {
+		cout << " @@@   @@@ " << '\n';
+		cout << "@   @ @   @" << '\n';
+		cout << "@    @    @" << '\n';
+		cout << "@         @" << '\n';
+		cout << " @       @ " << '\n';
+		cout << "  @     @  " << '\n';
+		cout << "   @   @   " << '\n';
+		cout << "    @ @    " << '\n';
+		cout << "     @     " << '\n';
+	}
+
+	return 0;
+}
