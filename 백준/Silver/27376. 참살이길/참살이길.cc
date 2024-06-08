c5eff276600934630bd30634d619940537248ac8
변성훈 <sphz3813@gmail.com>
2024-06-08 22:15:36 +0900

    [Silver III] Title: 참살이길, Time: 44 ms, Memory: 3196 KB -BaekjoonHub

--- 백준/Silver/27376. 참살이길/참살이길.cc
+++ 백준/Silver/27376. 참살이길/참살이길.cc
@@ -13,8 +13,8 @@ using namespace std;
 
 /****** VARIABLEs ******/
 int N, K;
-ll res = 0;
-tuple<ll, ll, ll> light[BOUND]; // [s, t]
+ll res = 0, is_green;
+tuple<int, int, int> light[BOUND]; // [x, s, t]
 /***********************/
 
 int main() {
@@ -23,32 +23,28 @@ int main() {
 
 	cin >> N >> K;
 
-	for (int i = 0; i < K; i++) {
-		ll x, t, s;
-
+	for (int i = 1; i <= K; i++) {
+		int x, t, s;
+        
 		cin >> x >> t >> s;
 		light[i] = { x, t, s };
 	}
 
-	sort(light, light + K); // 좌표순 정렬
+	sort(light + 1, light + (K + 1)); // 좌표순 정렬
 
-	for (int i = 0; i < K; i++) {
+	for (int i = 1; i <= K; i++) {
 		auto [x, t, s] = light[i];
 
-		if (i == 0)
-			res += x;
-		else
-			res += x - get<0>(light[i - 1]); // 현재 거리 - 이전 거리
-
-		ll is_green = (res - s) % (2 * t);
+	    res += x - get<0>(light[i - 1]); // 현재 거리 - 이전 거리
+		is_green = (res - s) % (2 * t);
 
 		if (is_green < 0) // s > res인 경우
 			res += s - res;
-		else if (t <= is_green && is_green < 2 * t)
+		else if (t <= is_green && is_green < 2 * t) // 신호등이 빨간불인 경우
 			res += (2 * t) - is_green;
 	}
 
-	res += N - get<0>(light[K - 1]); // 목적지 - 마지막 신호등 거리
+	res += N - get<0>(light[K]); // 목적지 - 마지막 신호등 거리
 	cout << res;
 
 	return 0;
