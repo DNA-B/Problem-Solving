3a3a223cc12d1f4b16b1340e835422b8d3e00902
변성훈 <sphz3813@gmail.com>
2024-07-01 09:08:35 +0900

    [Gold IV] Title: 이분 그래프, Time: 200 ms, Memory: 13536 KB -BaekjoonHub

--- 백준/Gold/1707. 이분 그래프/이분 그래프.cc
+++ 백준/Gold/1707. 이분 그래프/이분 그래프.cc
@@ -1,18 +1,20 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 
+using namespace std;
+
 /******** MACRO ********/
-#define BOUND 20001
-#define MOD 1'000
+#define BOUND 100001
+#define MOD 1'000'000'007
+#define PI 3.1415926535897932
 #define ll long long
 #define X first
 #define Y second
 /***********************/
 
-using namespace std;
-
 /****** VARIABLEs ******/
 int K, V, E;
+bool flag = false;
 int color[BOUND];
 vector<int> adj[BOUND];
 /***********************/
@@ -25,10 +27,17 @@ void init() {
 }
 
 void dfs(int cur) {
+	if (flag)
+		return;
+
 	for (int nxt : adj[cur]) {
 		if (!color[nxt]) {
 			color[nxt] = -color[cur]; // 자식은 부모 색이랑 반대 색으로 색칠
 			dfs(nxt);
+		}
+		else if (color[cur] == color[nxt]) { // 한 변의 양 끝점이 같은 색이라면 이분 그래프가 아니다.
+			flag = true;
+			return;
 		}
 	}
 }
@@ -45,12 +54,9 @@ bool solve() {
 			color[i] = 1;
 			dfs(i);
 		}
-	}
 
-	for (int i = 1; i <= V; i++) {
-		for (int nxt : adj[i])
-			if (color[i] == color[nxt]) // 한 변의 양 끝점이 같은 색이라면 이분 그래프가 아니다.
-				return false;
+		if (flag)
+			return false;
 	}
 
 	return true;
@@ -63,6 +69,7 @@ int main() {
 	cin >> K;
 
 	while (K--) {
+		flag = false;
 		cin >> V >> E;
 		init();
 		cout << (solve() ? "YES\n" : "NO\n");
