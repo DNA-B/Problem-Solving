5d5276336f0f0d3073be5957eed8e368231e8217
변성훈 <sphz3813@gmail.com>
2024-07-18 22:53:49 +0900

    [Bronze II] Title: 우리 집 밑에 편의점이 있는데, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/20155. 우리 집 밑에 편의점이 있는데/우리 집 밑에 편의점이 있는데.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/******** MACRO ********/
+#define BOUND 1001
+#define INF 0x3f3f3f3f
+#define MOD 1'000'000'007
+#define PI 3.1415926535897932
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int N, M;
+int arr[BOUND];
+/***********************/
+
+int main() {
+	cin.tie(NULL)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N >> M;
+
+	for (int x, i = 0; i < N; i++) {
+		cin >> x;
+		arr[x]++;
+	}
+
+	cout << *max_element(arr, arr + N);
+	return 0;
+}
