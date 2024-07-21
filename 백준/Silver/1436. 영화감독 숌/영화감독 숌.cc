ce46b770f1ac7708ed55c66c20423ee4c1a3e15a
변성훈 <sphz3813@gmail.com>
2024-07-22 03:41:31 +0900

    [Silver V] Title: 영화감독 숌, Time: 64 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.cc
@@ -2,26 +2,18 @@
 
 using namespace std;
 
-
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    int n;
-    int n_six = 0;
-    int n_cur = 0;
+    int n, n_six = 0, n_cur = 0;
     string tmp;
 
     cin >> n;
 
-    while (n_six != n)
-    {
-        tmp = to_string(n_cur);
-
-        if (tmp.find("666") != string::npos)
+    while (n_six != n) {
+        if (to_string(n_cur).find("666") != string::npos)
             n_six++;
-
         n_cur++;
     }
 
