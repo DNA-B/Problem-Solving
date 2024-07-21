797c4871c6d23185ef7f5f41a70ee64d6c16d06c
변성훈 <sphz3813@gmail.com>
2024-07-22 07:51:20 +0900

    [Gold IV] Title: 주간 미팅, Time: 4 ms, Memory: 2472 KB -BaekjoonHub

---
+++ 백준/Gold/12834. 주간 미팅/주간 미팅.cc
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/******** MACRO ********/
+#define INF 0x3f3f3f3f
+#define LLINF 1e18
+#define MOD 1'000'000'007
+#define PI 3.1415926535897932
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int N, V, E, KI, CR;
+int total_d = 0;
+int d[2][1001], home[101];
+vector<pair<int, int>> adj[1001];
+priority_queue<pair<int, int>,
+	vector<pair<int, int>>,
+	greater<pair<int, int>>> pq;
+/***********************/
+
+void dijk(int ki_or_cr) {
+	while (!pq.empty()) {
+		auto cur = pq.top();
+		pq.pop();
+
+		if (d[ki_or_cr][cur.X] != cur.Y)
+			continue;
+
+		for (auto nxt : adj[cur.X]) {
+			if (d[ki_or_cr][nxt.X] > d[ki_or_cr][cur.X] + nxt.Y) {
+				d[ki_or_cr][nxt.X] = d[ki_or_cr][cur.X] + nxt.Y;
+				pq.push({ nxt.X, d[ki_or_cr][nxt.X] });
+			}
+		}
+	}
+}
+
+int main() {
+	cin.tie(NULL)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N >> V >> E >> KI >> CR;
+
+	fill(d[0], d[0] + 1001, INF);
+	fill(d[1], d[1] + 1001, INF);
+	d[0][KI] = 0;
+	d[1][CR] = 0;
+
+	for (int i = 0; i < N; i++)
+		cin >> home[i];
+
+	for (int i = 0; i < E; i++) {
+		int a, b, len;
+		cin >> a >> b >> len;
+		adj[a].push_back({ b, len });
+		adj[b].push_back({ a, len });
+	}
+
+	pq.push({ KI, d[0][KI] });
+	dijk(0);
+	pq.push({ CR, d[1][CR] });
+	dijk(1);
+
+	for (int rep = 0; rep < 2; rep++) {
+		for (int i = 0; i < N; i++)
+			total_d += (d[rep][home[i]] == INF ? -1 : d[rep][home[i]]);
+	}
+
+	cout << total_d;
+	return 0;
+}
