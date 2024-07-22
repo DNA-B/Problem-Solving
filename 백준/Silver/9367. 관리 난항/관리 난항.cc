0f9adbb08071c85c7cef572a94f5f9e761caa529
변성훈 <sphz3813@gmail.com>
2024-07-23 08:42:34 +0900

    [Silver II] Title: 관리 난항, Time: 64 ms, Memory: 2440 KB -BaekjoonHub

--- 백준/Silver/9367. 관리 난항/관리 난항.cc
+++ 백준/Silver/9367. 관리 난항/관리 난항.cc
@@ -25,24 +25,21 @@ int main() {
 
 	while (T--) {
 		map<string, tuple<int, int, int>> car; // {차종 <원가, 대여비, 1km당 추가 비용>}
-		map<string, ll> bill;
-		map<string, int> state; // state가 -1이면 INCONSISTENT, 0이면 p, 1이면 a, 2면 r
+		map<string, int> bill, state; // state가 -1이면 INCONSISTENT, 0이면 p, 1이면 a, 2면 r
 		map<string, string> rent_car;
 
 		cin >> N >> M;
 
+        string cname;
+		int price, rental, per;
 		for (int i = 0; i < N; i++) {
-			string cname;
-			int price, rental, per;
-
 			cin >> cname >> price >> rental >> per;
 			car[cname] = { price, rental, per };
 		}
 
+        string aname, some, akind;
+		int atime;
 		for (int i = 0; i < M; i++) {
-			string aname, some, akind;
-			int atime;
-
 			cin >> atime >> aname >> akind >> some; // p면 some은 차종, a면 파손율, r이면 주행 거리
 
 			if (state.find(aname) != state.end() && state[aname] == -1)
@@ -67,7 +64,7 @@ int main() {
 					continue;
 				}
 				state[aname] = 1;
-				bill[aname] += (get<0>(car[rent_car[aname]]) * stoll(some) + 99) / 100; // 원가 * 파손율;
+				bill[aname] += ceil(get<0>(car[rent_car[aname]]) * stod(some) / 100); // 원가 * 파손율;
 			}
 			else if (akind == "r") {
 				if (state[aname] == 2) {
@@ -75,7 +72,7 @@ int main() {
 					continue;
 				}
 				state[aname] = 2;
-				bill[aname] += get<2>(car[rent_car[aname]]) * stoll(some);
+				bill[aname] += get<2>(car[rent_car[aname]]) * stoi(some);
 			}
 		}
 
