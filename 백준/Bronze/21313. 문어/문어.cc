988f69eac0cca4b6f9deb211d6d6bd1310130617
변성훈 <sphz3813@gmail.com>
2024-07-23 09:24:09 +0900

    [Bronze II] Title: 문어, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21313. 문어/문어.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/******** MACRO ********/
+#define INF 0x3f3f3f3f
+#define LLINF 1e18
+#define MOD 1'000'000'007
+#define PI 3.1415926535897932
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int N;
+/***********************/
+
+int main() {
+	cin.tie(NULL)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N;
+
+	for (int i = 0; i < N - 1; i++)
+		cout << (i & 1 ? "2 " : "1 ");
+	cout << (N & 1 ? '3' : '2');
+
+	return 0;
+}
