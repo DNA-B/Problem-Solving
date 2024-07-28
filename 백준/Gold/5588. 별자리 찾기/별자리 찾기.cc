c01c52150df39eb76091be4201fadf79d0a3631c
변성훈 <sphz3813@gmail.com>
2024-07-28 12:42:20 +0900

    [Gold V] Title: 별자리 찾기, Time: 104 ms, Memory: 14572 KB -BaekjoonHub

---
+++ 백준/Gold/5588. 별자리 찾기/별자리 찾기.cc
@@ -0,0 +1,53 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/******** MACRO ********/
+#define INF 0x3f3f3f3f
+#define LLINF 1e18
+#define MOD 1'000'000'007
+#define PI 3.1415926535897932
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int N, M, x, y;
+pair<int, int> wanted_star[201], star[1001];
+map<pair<int, int>, int> m; // { {x 차이, y 차이}, cnt }
+/***********************/
+
+int main() {
+	cin.tie(NULL)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> M;
+
+	for (int i = 0; i < M; i++) {
+		cin >> x >> y;
+		wanted_star[i] = { x, y };
+	}
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y;
+		star[i] = { x, y };
+	}
+
+	for (int i = 0; i < M; i++) {
+		for (int j = 0; j < N; j++) {
+			pair<int, int> p = { star[j].X - wanted_star[i].X, star[j].Y - wanted_star[i].Y };
+			m[p]++;
+
+			if (m[p] == M) {
+				cout << p.X << " " << p.Y;
+				exit(0);
+			}
+		}
+	}
+
+	return 0;
+}
