f2e3e04b3f814a103cfeb2dac8005d26ca96de83
변성훈 <sphz3813@gmail.com>
2024-07-29 23:46:40 +0900

    [Bronze IV] Title: Celebrity jeopardy, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/3765. Celebrity jeopardy/Celebrity jeopardy.cc
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+/******** MACRO ********/
+#define INF 0x3f3f3f3f
+#define LLINF 1e18
+#define MOD 1'000'000'007
+#define PI 3.1415926535897932
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+string S;
+/***********************/
+
+int main() {
+	cin.tie(NULL)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	while (getline(cin, S))
+		cout << S << "\n";
+
+	return 0;
+}
