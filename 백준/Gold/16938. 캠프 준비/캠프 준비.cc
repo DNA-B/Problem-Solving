fa03f461795d4957e27b4ca9db050a8224c40220
변성훈 <sphz3813@gmail.com>
2024-08-03 16:17:52 +0900

    [Gold V] Title: 캠프 준비, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.cc
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.cc
@@ -4,17 +4,14 @@
 #include <stdio.h>
 using namespace std;
 int N, L, R, X, res = 0;
-int diff[20], vis[20], arr[20];
+int diff[20], arr[20];
 void solve(int cnt, int idx) {
 	if (cnt > N) return;
 	if (cnt >= 2 && arr[cnt - 1] - arr[0] >= X) {
 		long long total = accumulate(arr, arr + cnt, 0LL);
 		if (L <= total && total <= R) res++;
 		else if (total > R) return;}
-	for (int i = idx; i < N; i++) {
-		if (!vis[i]) {
-			vis[i] = 1;arr[cnt] = diff[i];solve(cnt + 1, i + 1);vis[i] = 0;
-		}}}
+	for (int i = idx; i < N; i++) {arr[cnt] = diff[i];solve(cnt + 1, i + 1);}}
 int main() {
 	scanf("%d %d %d %d", &N, &L, &R, &X);
 	for (int i = 0; i < N; i++)scanf("%d", &diff[i]);
