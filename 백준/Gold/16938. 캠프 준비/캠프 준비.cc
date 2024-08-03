bc90e2129b9e73dee8277cafaefca05b32e8f6ab
변성훈 <sphz3813@gmail.com>
2024-08-03 16:09:30 +0900

    [Gold V] Title: 캠프 준비, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.cc
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.cc
@@ -3,52 +3,20 @@
 #include <numeric>
 #include <stdio.h>
 using namespace std;
-
-/******** MACRO ********/
-#define INF 0x3f3f3f3f
-#define LLINF 1e18
-#define MOD 1'000'000'007
-#define PI 3.1415926535897932
-#define ll long long
-#define X first
-#define Y second
-/***********************/
-
-/****** VARIABLEs ******/
 int N, L, R, X, res = 0;
 int diff[20], vis[20], arr[20];
-/***********************/
-
 void solve(int cnt, int idx) {
-	if (cnt > N)
-		return;
+	if (cnt > N) return;
 	if (cnt >= 2 && arr[cnt - 1] - arr[0] >= X) {
-		ll total = accumulate(arr, arr + cnt, 0LL);
-
-		if (L <= total && total <= R)
-			res++;
-		else if (total > R) // total이 R보다 크면 뒤에 어떤 수를 추가해도 R보다 큼
-			return;
-	}
-
+		long long total = accumulate(arr, arr + cnt, 0LL);
+		if (L <= total && total <= R) res++;
+		else if (total > R) return;}
 	for (int i = idx; i < N; i++) {
 		if (!vis[i]) {
-			vis[i] = 1;
-			arr[cnt] = diff[i];
-			solve(cnt + 1, i + 1);
-			vis[i] = 0;
-		}
-	}
-}
-
+			vis[i] = 1;arr[cnt] = diff[i];solve(cnt + 1, i + 1);vis[i] = 0;
+		}}}
 int main() {
 	scanf("%d %d %d %d", &N, &L, &R, &X);
-
-	for (int i = 0; i < N; i++)
-		scanf("%d", &diff[i]);
-
-	sort(diff, diff + N);
-	solve(0, 0);
-	printf("%d", res);
-	return 0;
-}
+	for (int i = 0; i < N; i++)scanf("%d", &diff[i]);
+	sort(diff, diff + N);solve(0, 0);printf("%d", res);
+	return 0;}
