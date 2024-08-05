a854de3d384c41d341af92aba3ad5a75b1d119bc
변성훈 <sphz3813@gmail.com>
2024-08-05 23:12:55 +0900

    [Gold V] Title: 택배 배송, Time: 24 ms, Memory: 5532 KB -BaekjoonHub

--- 백준/Gold/5972. 택배 배송/택배 배송.cc
+++ 백준/Gold/5972. 택배 배송/택배 배송.cc
@@ -1,64 +1,33 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 using namespace std;
-
-/******** MACRO ********/
 #define INF 0x3f3f3f3f
-#define LLINF 1e18
-#define MOD 1'000'000'007
-#define PI 3.1415926535897932
-#define ll long long
-#define X first
-#define Y second
-/***********************/
-
 /****** VARIABLEs ******/
 int N, M;
 int d[50001];
 vector<pair<int, int>> adj[50001];
-priority_queue<pair<int, int>,
-	vector<pair<int, int>>,
-	greater<pair<int, int>>> pq;
+priority_queue<pair<int, int>,vector<pair<int, int>>,greater<pair<int, int>>> pq;
 /***********************/
-
 int dijk(int st) {
-	d[st] = 0;
-	pq.push({ 0, st });
-
+	d[st] = 0;pq.push({ 0, st });
 	while (!pq.empty()) {
-		auto [cur_cost, cur] = pq.top();
-		pq.pop();
-
-		if (d[cur] != cur_cost)
-			continue;
-		if (cur == N)
-			break;
-
+		auto [cur_cost, cur] = pq.top();pq.pop();
+		if (d[cur] != cur_cost)continue;
+		if (cur == N)break;
 		for (auto [nxt_cost, nxt] : adj[cur]) {
 			if (d[nxt] > d[cur] + nxt_cost) {
 				d[nxt] = d[cur] + nxt_cost;
 				pq.push({ d[nxt], nxt });
-			}
-		}
-	}
-
-	return d[N];
-}
-
+			}}}
+	return d[N];}
 int main() {
 	cin.tie(NULL)->ios_base::sync_with_stdio(false);
-	// freopen("input.txt", "r", stdin);
-
 	cin >> N >> M;
 	fill(d + 1, d + (N + 1), INF);
-
     int u, v, cost;
 	for (int i = 0; i < M; i++) {
 		cin >> u >> v >> cost;
 		adj[u].push_back({ cost,  v });
 		adj[v].push_back({ cost,  u });
 	}
-
 	cout << dijk(1);
-	return 0;
-}
+	return 0;}
