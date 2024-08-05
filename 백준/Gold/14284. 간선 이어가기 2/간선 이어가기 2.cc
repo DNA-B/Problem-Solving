40187b8f2ee5d34b06995c2631cdf0b4d224849e
변성훈 <sphz3813@gmail.com>
2024-08-05 23:19:49 +0900

    [Gold V] Title: 간선 이어가기 2, Time: 24 ms, Memory: 4844 KB -BaekjoonHub

--- 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.cc
+++ 백준/Gold/14284. 간선 이어가기 2/간선 이어가기 2.cc
@@ -4,60 +4,54 @@
 using namespace std;
 
 /******** MACRO ********/
-#define BOUND 100001
 #define MOD 1'000'000'007
 #define PI 3.1415926535897932
 #define ll long long
 #define X first
 #define Y second
 /***********************/
-
-/****** VARIABLEs ******/
 const int INF = 0x3f3f3f3f;
+/****** VARIABLEs ******/
 int N, M, S, T;
-int d[BOUND];
-vector<pair<int, int>> adj[BOUND];
+int d[5005];
+vector<pair<int, int>> adj[5005];
 priority_queue<pair<int, int>,
 	vector<pair<int, int>>,
 	greater<pair<int, int>>> pq;
 /***********************/
 
-void dijk() {
-	fill(d, d + (N + 1), INF);
-	d[S] = 0;
-	pq.push({ S, d[S] });
-
-	while (!pq.empty()) {
-		auto cur = pq.top();
-		pq.pop();
-
-		if (d[cur.X] != cur.Y)
-			continue;
-
-		for (auto nxt : adj[cur.X]) {
-			if (d[nxt.X] >= d[cur.X] + nxt.Y) {
-				d[nxt.X] = d[cur.X] + nxt.Y;
-				pq.push({ nxt.X, d[nxt.X] });
-			}
-		}
-	}
-}
-
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	// freopen("input.txt", "r", stdin);
-
+	cin.tie(NULL)->ios_base::sync_with_stdio(false);
 	cin >> N >> M;
 
-	for (int u, v, cost, i = 0; i < M; i++) {
+	for (int i = 0; i < M; i++) {
+		int u, v, cost;
 		cin >> u >> v >> cost;
-		adj[u].push_back({ v, cost });
-		adj[v].push_back({ u, cost });
+		adj[u].push_back({ cost, v });
+		adj[v].push_back({ cost, u });
 	}
 
 	cin >> S >> T;
-	dijk();
+
+	fill(d, d + (N + 1), INF);
+	d[S] = 0;
+	pq.push({ d[S], S });
+
+	while (!pq.empty()) {
+		auto [cur_cost, cur] = pq.top();
+		pq.pop();
+
+		if (d[cur] != cur_cost)
+			continue;
+
+		for (auto [nxt_cost, nxt] : adj[cur]) {
+			if (d[nxt] > d[cur] + nxt_cost) {
+				d[nxt] = d[cur] + nxt_cost;
+				pq.push({ d[nxt], nxt });
+			}
+		}
+	}
+
 	cout << d[T];
 
 	return 0;
