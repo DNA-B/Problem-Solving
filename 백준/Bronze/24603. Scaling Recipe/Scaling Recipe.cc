8cd366ece0d9c04d5b4efba19cd164a04b64c789
변성훈 <sphz3813@gmail.com>
2024-08-14 11:23:18 +0900

    [Bronze III] Title: Scaling Recipe, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24603. Scaling Recipe/Scaling Recipe.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+/******** MACRO ********/
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define MOD 1'000'000'007
+#define PI 3.1415926535897932
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int N, X, Y;
+/***********************/
+
+int main() {
+	cin.tie(NULL)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N >> X >> Y;
+
+	for (int i = 0; i < N; i++) {
+		ll a;
+		cin >> a;
+		cout << (a * (Y / X)) + (((Y % X) * a) / X) << '\n';
+	}
+
+	return 0;
+}
