07b61696e1ecac8bc09a5387c190994e6e973447
변성훈 <sphz3813@gmail.com>
2024-08-15 15:23:43 +0900

    [Silver I] Title: 단어 맞추기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/9081. 단어 맞추기/단어 맞추기.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+/******** MACRO ********/
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define MOD 1'000'000'007
+#define PI 3.1415926535897932
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int N;
+string S;
+/***********************/
+
+int main() {
+	cin.tie(NULL)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N;
+
+	while (N--) {
+		cin >> S;
+		next_permutation(S.begin(), S.end());
+		if (is_sorted(S.begin(), S.end()))
+			prev_permutation(S.begin(), S.end());
+		cout << S << '\n';
+
+	}
+
+	return 0;
+}
