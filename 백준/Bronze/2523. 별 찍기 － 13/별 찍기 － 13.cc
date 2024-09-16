b35da91addb560f71bf7dcadf3adf5765485289b
변성훈 <sphz3813@gmail.com>
2024-09-16 17:28:04 +0900

    [Bronze III] Title: 별 찍기 - 13, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2523. 별 찍기 － 13/별 찍기 － 13.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+/******** MACRO ********/
+#define PI 3.1415926535897932
+#define MOD 1000000007
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int N;
+/***********************/
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N;
+
+	for (int i = 1; i <= N; i++) {
+		for (int j = 0; j < i; j++)
+			cout << '*';
+		cout << '\n';
+	}
+
+	for (int i = N; i < 2 * N - 1; i++) {
+		for (int j = 0; j < (2 * N - 1) - i; j++)
+			cout << '*';
+		cout << '\n';
+	}
+
+	return 0;
+}
