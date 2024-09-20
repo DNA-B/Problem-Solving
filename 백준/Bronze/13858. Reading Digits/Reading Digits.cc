5451b526616f0096698acd24e17d3615cc1d9a0d
변성훈 <sphz3813@gmail.com>
2024-09-20 18:43:32 +0900

    [Bronze I] Title: Reading Digits, Time: 4 ms, Memory: 2884 KB -BaekjoonHub

---
+++ 백준/Bronze/13858. Reading Digits/Reading Digits.cc
@@ -0,0 +1,41 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+/******** MACRO ********/
+#define PI 3.1415926535897932
+#define MOD 1000000007
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int k, pos;
+string cur;
+/***********************/
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> k >> pos >> cur;
+
+	while (k--) {
+		string tmp = "";
+
+		for (int i = 0; i < cur.size(); i++) {
+			if (i & 1)
+				continue;
+
+			for (int rep = 0; rep < cur[i] - '0'; rep++)
+				tmp += cur[i + 1];
+		}
+		cur = tmp;
+	}
+
+	cout << cur[pos];
+	return 0;
+}
