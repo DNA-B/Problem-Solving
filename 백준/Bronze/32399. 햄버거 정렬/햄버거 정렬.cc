b5de04b9e3826296ee43d29c1279c26142825a5b
변성훈 <sphz3813@gmail.com>
2024-09-29 20:08:20 +0900

    [Bronze III] Title: 햄버거 정렬, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/32399. 햄버거 정렬/햄버거 정렬.cc
@@ -0,0 +1,33 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+/******** MACRO ********/
+#define PI 3.1415926535897932
+#define MOD 1000000007
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+string S;
+/***********************/
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> S;
+
+	if (S == "(1)")
+		cout << 0;
+	else if (S == ")1(")
+		cout << 2;
+	else
+		cout << 1;
+
+	return 0;
+}
