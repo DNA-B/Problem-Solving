fe2f530c224653557a4b926abeaeda3e1d553620
변성훈 <sphz3813@gmail.com>
2024-10-07 21:38:34 +0900

    [Bronze IV] Title: 모범생 포닉스, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28097. 모범생 포닉스/모범생 포닉스.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+
+using namespace std;
+
+/******** MACRO ********/
+#define PI 3.1415926535897932
+#define MOD 1000000007
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
+int N, T, total = 0;
+/***********************/
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N;
+	total += 8 * (N - 1);
+
+	while (N--) {
+		cin >> T;
+		total += T;
+	}
+
+	cout << (total / 24) << ' ' << (total % 24);
+	return 0;
+}
