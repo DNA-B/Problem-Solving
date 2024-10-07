f699e1627544643775187a1836d52e56e25f7e50
변성훈 <sphz3813@gmail.com>
2024-10-07 21:31:19 +0900

    [Bronze V] Title: A+B - 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.cc
@@ -1,29 +1,14 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
-
 using namespace std;
-
-/******** MACRO ********/
-#define PI 3.1415926535897932
-#define MOD 1000000007
-#define INT_INF 0x3f3f3f3f
-#define LL_INF 1e18
-#define ll long long
-#define X first
-#define Y second
-/***********************/
-
 /****** VARIABLEs ******/
-pair<int, int> n;
+int a, b;
 /***********************/
-
 int main() {
 	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
 	// freopen("input.txt", "r", stdin);
 
-	while (cin >> n.X >> n.Y) {
-		cout << n.X + n.Y << '\n';
-	}
+	while (cin >> a >> b)
+		cout << a + b << '\n';
 
 	return 0;
 }
