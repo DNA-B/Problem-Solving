7c18176c51f46171d4e7f222680d0fc3a6b8f0e2
변성훈 <sphz3813@gmail.com>
2024-10-28 19:04:31 +0900

    [Bronze I] Title: 달팽이는 올라가고 싶다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
+++ 백준/Bronze/2869. 달팽이는 올라가고 싶다/달팽이는 올라가고 싶다.cc
@@ -2,23 +2,14 @@
 
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main() {
+    cin.tie(0)->ios::sync_with_stdio(0);
 
-    long long a, b, v;
-    long long res;
-    long long day_height;
-
+    long long a, b, v, res;
     cin >> a >> b >> v;
-    day_height = a - b;
 
-    if ((v - a) % (a - b) == 0) // a - b는 미끄러진 것 포함이므로 v에서 미리 a를 뺀 값으로 계산(res + 1)
-        res = (v - a) / (a - b) ;
-    else
-        res = (v - a) / (a - b) + 1;
-
+    // a - b는 미끄러진 것 포함이므로 v에서 미리 a를 뺀 값으로 계산(res + 1)
+    res = (v - a) % (a - b) == 0 ? (v - a) / (a - b) : (v - a) / (a - b) + 1;
     cout << res + 1;
 
     return 0;
