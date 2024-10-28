cf92225b8d16942f18726de30dab00ca92032c43
변성훈 <sphz3813@gmail.com>
2024-10-28 23:29:16 +0900

    [Silver V] Title: 나이순 정렬, Time: 64 ms, Memory: 9848 KB -BaekjoonHub

--- 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
+++ 백준/Silver/10814. 나이순 정렬/나이순 정렬.cc
@@ -2,13 +2,11 @@
 
 using namespace std;
 
-bool compare(pair<int, string> v1, pair<int, string> v2)
-{
+bool compare(pair<int, string> v1, pair<int, string> v2) {
 	return	v1.first < v2.first;
 }
 
-int main()
-{
+int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
 
@@ -19,8 +17,7 @@ int main()
 
 	cin >> n;
 
-	while (n--)
-	{
+	while (n--) {
 		cin >> age >> name;
 		v.push_back({ age, name });
 	}
@@ -28,9 +25,7 @@ int main()
 	stable_sort(v.begin(), v.end(), compare); // stable_sort는 같을 때 순서를 유지한다.
 	
 	for (int i = 0; i < v.size(); i++)
-	{
 		cout << v[i].first << " " << v[i].second << "\n";
-	}
 
 	return 0;
 }
