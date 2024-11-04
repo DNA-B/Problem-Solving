26e8faa3b13fa2b3a6eaf06c4d9db86075cf2a82
변성훈 <sphz3813@gmail.com>
2024-11-05 00:37:50 +0900

    [Silver II] Title: 베르트랑 공준, Time: 4 ms, Memory: 2272 KB -BaekjoonHub

--- 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
+++ 백준/Silver/4948. 베르트랑 공준/베르트랑 공준.cc
@@ -4,22 +4,18 @@ using namespace std;
 
 bool is_not_prime[246913];
 
-void era(int n)
-{
+void era(int n) {
     is_not_prime[1] = true;
 
-    for (int i = 2; i < sqrt(n); i++)
-    {
+    for (int i = 2; i <= sqrt(n); i++) {
         if (is_not_prime[i])
             continue;
-
         for (int j = i + i; j <= n; j += i)
             is_not_prime[j] = true;
     }
 }
 
-int main()
-{
+int main() {
     ios::sync_with_stdio(0);
     cin.tie(0);
 
@@ -28,15 +24,13 @@ int main()
 
     era(246913);
 
-    while(1)
-    {
+    while(1) {
         cin >> n;
 
         if (n == 0)
             return 0;
 
-        for (int i = n + 1; i <= 2 * n; i++)
-        {
+        for (int i = n + 1; i <= 2 * n; i++) {
             if (!is_not_prime[i])
                 cnt++;
         }
