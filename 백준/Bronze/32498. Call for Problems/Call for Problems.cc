7aea496e750b93da2d17786b8b398f2d90d81891
변성훈 <sphz3813@gmail.com>
2024-11-06 21:49:28 +0900

    [Bronze V] Title: Call for Problems, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/32498. Call for Problems/Call for Problems.cc
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, d, cnt = 0;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> N;
+
+	while (N--) {
+		cin >> d;
+		cnt += (d % 2);
+	}
+
+	cout << cnt;
+	return 0;
+}
