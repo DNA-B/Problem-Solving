87ed9e09c15395882bef58bf65196efed789215b
변성훈 <sphz3813@gmail.com>
2024-11-11 23:05:00 +0900

    [Silver IV] Title: 괄호, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.cc
+++ 백준/Silver/9012. 괄호/괄호.cc
@@ -2,52 +2,38 @@
 
 using namespace std;
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+    cin.tie(0)->ios::sync_with_stdio(0);
 
 	string str;
-	int count = 0;
-	int n;
+	int n, count = 0;
 	bool yes = true;
 
 	cin >> n;
 
-	while (n--)
-	{
+	while (n--) {
 		stack<char> st;
 		yes = true;
 
 		cin >> str;
 
-		for (int i = 0; i < str.size(); i++)
-		{
-			if (str[i] == '(') st.push(str[i]);
-			else if (str[i] == ')')
-			{
+		for (int i = 0; i < str.size(); i++) {
+			if (str[i] == '(') 
+                st.push(str[i]);
+			else if (str[i] == ')') {
 				if (!st.empty() && st.top() == '(')
-				{
 					st.pop();
-				}
-				else
-				{
+				else {
 					yes = false;
 					break;
 				}
 			}
 		}
 
-		if (!st.empty()) yes = false;
+		if (!st.empty()) 
+            yes = false;
 
-		if (yes)
-		{
-			cout << "YES\n";
-		}
-		else
-		{
-			cout << "NO\n";
-		}
+        cout << (yes ? "YES\n" : "NO\n");
 	}
 
 	return 0;
