838e1be51cf2846563923249223b7438d02dbf82
변성훈 <sphz3813@gmail.com>
2024-11-12 12:06:07 +0900

    [Silver III] Title: 풍선 터뜨리기, Time: 0 ms, Memory: 2156 KB -BaekjoonHub

--- 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
+++ 백준/Silver/2346. 풍선 터뜨리기/풍선 터뜨리기.cc
@@ -2,31 +2,24 @@
 
 using namespace std;
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+int main() {
+    cin.tie(0)->ios::sync_with_stdio(0);
 
-    int n, x;
-    int cur;
+    int n, x, cur;
     vector <pair<int, int>> v;
 
     cin >> n;
 
-    for (int i = 1; i <= n; i++)
-    {
+    for (int i = 1; i <= n; i++) {
         cin >> x;
         v.push_back({ i,x });
     }
 
     auto it = v.begin();
 
-    while (!v.empty())
-    {
+    while (!v.empty()) {
         cur = it->second;
-
         cout << it->first << " ";
-
         it = v.erase(it);
 
         if (v.size() == 0)
@@ -35,23 +28,17 @@ int main()
         if (it == v.end())
             it = v.begin();
 
-        if (cur < 0)
-        {
-            for (int i = cur; i < 0; i++)
-            {
+        if (cur < 0){
+            for (int i = cur; i < 0; i++) {
                 if (it == v.begin())
                     it = v.end();
-
                 it--; 
             }
         }
-        else
-        {
-            for (int i = 0; i < cur - 1; i++)
-            {
+        else {
+            for (int i = 0; i < cur - 1; i++) {
                 if (it == v.end())
                     it = v.begin();
-
                 it++;
             }
         }
