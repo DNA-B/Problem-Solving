d3dce4bd2b66dedc05ee8d286a6f2d11c53aa18a
변성훈 <sphz3813@gmail.com>
2024-11-13 14:51:35 +0900

    [Gold IV] Title: 텔레포트, Time: 8 ms, Memory: 5976 KB -BaekjoonHub

--- 백준/Gold/16958. 텔레포트/텔레포트.cc
+++ 백준/Gold/16958. 텔레포트/텔레포트.cc
@@ -17,15 +17,35 @@ bool is_special[1005];
 pair<int, int> cities[1005];
 //=========================//
 
-void solve() {
-	for (int k = 1; k <= N; k++) {
-		for (int st = 1; st <= N; st++) {
-			for (int en = 1; en <= N; en++) {
-				if (dist[st][en] > dist[st][k] + dist[k][en])
-					dist[st][en] = dist[st][k] + dist[k][en];
-			}
+int find_mn(int x) {
+	int mn = INF, mn_idx = INF;
+
+	for (int i = 1; i <= N; i++) {
+		if (!is_special[i])
+			continue;
+
+		if (mn > dist[x][i]) {
+			mn = dist[x][i];
+			mn_idx = i;
 		}
 	}
+
+	return mn_idx;
+}
+
+int solve(int st, int en) {
+	int direct = dist[st][en];
+
+	// 둘 다 특별한 도시
+	if (is_special[st] && is_special[en])
+		direct = min(direct, T);
+
+	// 한번에 가는 것과 거쳤다 오는 것 비교
+	int near_st = find_mn(st);
+	int near_en = find_mn(en);
+	direct = min(direct, dist[st][near_st] + T + dist[near_en][en]);
+
+	return direct;
 }
 
 int main() {
@@ -39,21 +59,16 @@ int main() {
 	for (int i = 1; i <= N; i++)
 		cin >> is_special[i] >> cities[i].X >> cities[i].Y;
 
-	for (int i = 1; i < N; i++) {
+	for (int i = 1; i <= N; i++) {
 		dist[i][i] = 0;
-		for (int j = i + 1; j <= N; j++) {
+		for (int j = i + 1; j <= N; j++)
 			dist[i][j] = dist[j][i] = abs(cities[i].X - cities[j].X) + abs(cities[i].Y - cities[j].Y);
-			if (is_special[i] && is_special[j] && dist[i][j] > T) // 둘 다 특별한 도시고 거리가 T보다 클 때 텔레포트
-				dist[i][j] = dist[j][i] = T;
-		}
 	}
-
-	solve(); // 플로이드
 
 	cin >> M;
 	for (int i = 0; i < M; i++) {
 		cin >> u >> v;
-		cout << dist[u][v] << '\n';
+		cout << solve(u, v) << '\n';
 	}
 
 	return 0;
