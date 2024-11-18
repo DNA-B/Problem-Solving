5432b84a2fcb85b1b1b7efe1545661c82d6e65e4
변성훈 <sphz3813@gmail.com>
2024-11-18 21:14:06 +0900

    [Bronze I] Title: 약수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1037. 약수/약수.cc
+++ 백준/Bronze/1037. 약수/약수.cc
@@ -1,42 +1,18 @@
-#include <iostream>
-#include <cctype>
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <algorithm>
-#include <string>
-#include <queue>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-//
-int main()
-{
+int main() {
 	int n;
-	int real[50] = { 0 };
-	int max = -1;
-	int min = 1000001;
-	int result = 0;
+    vector<int> v;
 	
-
 	cin >> n;
 
-	for (int i = 0; i < n; i++)
-	{
-		cin >> real[i];
-
-		if (real[i] > max)
-		{
-			max = real[i];
-		}
-
-		if (real[i] < min)
-		{
-			min = real[i];
-		}
+	for (int x, i = 0; i < n; i++) {
+		cin >> x;
+        v.push_back(x);
 	}
-
-	cout << max * min;
-
-	return 0;
+    
+    sort(v.begin(), v.end());
+	cout << v.front() * v.back();
 }
