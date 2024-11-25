3509e31b0c327dd6d81fef7736f12328176e2870
변성훈 <sphz3813@gmail.com>
2024-11-26 08:19:05 +0900

    [Gold IV] Title: SHOW ME THE DUNGEON, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/25330. SHOW ME THE DUNGEON/SHOW ME THE DUNGEON.cc
+++ 백준/Gold/25330. SHOW ME THE DUNGEON/SHOW ME THE DUNGEON.cc
@@ -1,56 +1,47 @@
-#define _CRT_SECURE_NO_WARNINGS
-#include <bits/stdc++.h>
+#include <iostream>
+#include <algorithm>
+
 using namespace std;
-//========= MACRO =========//
-#define PI 3.1415926535897932
-#define INF 0x3f3f3f3f
-#define LL_INF 1e18
-#define ll long long
-#define X first
-#define Y second
-//=========================//
 
-//======= VARIABLEs =======//
-int N, K, mx = 0;
-bool chk[25];
-pair<int, int>town[25];
-//=========================//
+#define fastIO (ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL))
+#define nl '\n'
 
-void solve(int cur, int damage, int save) {
-	if (save > mx)
-		mx = save;
-	if (cur == 0)
-		return;
+struct AP {
+	int atk;
+	int pop;
+};
 
-	int remain = 0;
-	for (int i = 0; i < N; i++) {
-		auto [attack, people] = town[i];
-		if (!chk[i] && damage + attack <= cur)
-			remain += people;
-	}
+int N, K;
+AP ap[20];
+int maxPop;
 
-	if (save + remain <= mx) // 현재 체력으로 갈 수 있는 모든 마을의 생존자를 합쳐도 최대보다 작으면 return;
-		return;
+void Find(int prefixSum, int fatigability, int totalPop, int index) {
+	for (int i = index; i < N; i++) {
+		int nextFatigability = fatigability + ap[i].atk;
+		int nextTotalPop = totalPop + ap[i].pop;
+		int nextPrefixSum = prefixSum + nextFatigability;
+		if (nextPrefixSum > K) break;
 
-	for (int i = 0; i < N; i++) {
-		auto [attack, people] = town[i];
-		if (!chk[i] && damage + attack <= cur) {
-			chk[i] = true;
-			solve(cur - (damage + attack), damage + attack, save + people);
-			chk[i] = false;
-		}
+		maxPop = max(maxPop, nextTotalPop);
+		Find(nextPrefixSum, nextFatigability, nextTotalPop, i + 1);
 	}
 }
 
 int main() {
-	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
-	// freopen("input.txt", "r", stdin);
+	fastIO;
 
 	cin >> N >> K;
+	for (int i = 0; i < N; i++)
+		cin >> ap[i].atk;
+	for (int i = 0; i < N; i++)
+		cin >> ap[i].pop;
 
-	for (int i = 0; i < N; i++) cin >> town[i].X;
-	for (int i = 0; i < N; i++) cin >> town[i].Y;
+	sort(ap, ap + N, [](AP &a, AP &b) {
+		return a.atk < b.atk;
+	});
 
-	solve(K, 0, 0);
-	cout << mx;
+	Find(0, 0, 0, 0);
+	cout << maxPop;
+
+	return 0;
 }
