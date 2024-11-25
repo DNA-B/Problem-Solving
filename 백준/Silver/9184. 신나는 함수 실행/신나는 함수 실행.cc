0253afcfc0b3a6225673a08214dd75599ee52ded
변성훈 <sphz3813@gmail.com>
2024-11-25 21:28:13 +0900

    [Silver II] Title: 신나는 함수 실행, Time: 8 ms, Memory: 3056 KB -BaekjoonHub

--- 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
+++ 백준/Silver/9184. 신나는 함수 실행/신나는 함수 실행.cc
@@ -1,41 +1,23 @@
 #define _CRT_SECURE_NO_WARNINGS
-
-#include <iostream> // C++ library
-#include <algorithm>
-#include <string>
-#include <cctype> // C library
-#include <cstring>
-#include <cstdlib>
-#include <cmath>
-#include <vector> // STL
-#include <queue>
-#include <stack>
-#include <list>
+#include <bits/stdc++.h>
 
 using namespace std;
 
-long long mem[51][51][51] = { {0} };
+long long mem[51][51][51];
 
-long long w(int a, int b, int c)
-{
+long long w(int a, int b, int c) {
 	if (a <= 0 || b <= 0 || c <= 0)
-	{
 		return 1;
-	}
 	
 	if (mem[a][b][c] != 0)
-	{
 		return mem[a][b][c];
-	}
 
-	if (a > 20 || b > 20 || c > 20)
-	{
+	if (a > 20 || b > 20 || c > 20) {
 		mem[a][b][c] = w(20, 20, 20);
 		return mem[a][b][c];
 	}
 
-	if (a < b && b < c)
-	{
+	if (a < b && b < c) {
 		mem[a][b][c] =  w(a, b, c - 1) + w(a, b - 1, c - 1) - w(a, b - 1, c);
 		return mem[a][b][c];
 	}
@@ -44,24 +26,16 @@ long long w(int a, int b, int c)
 	return mem[a][b][c];
 }
 
-
-
-int main()
-{
+int main() {
 	int a, b, c;
 
-	while (1)
-	{
+	while (1) {
 		scanf("%d %d %d", &a, &b, &c);
 
 		if (a == -1 && b == -1 && c == -1)
-		{
 			return 0;
-		}
 		else
-		{
 			printf("w(%d, %d, %d) = %lld\n", a, b, c, w(a, b, c));
-		}
 	}
 	
 	return 0;
