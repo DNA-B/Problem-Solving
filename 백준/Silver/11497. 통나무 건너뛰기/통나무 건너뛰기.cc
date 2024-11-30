1eeeaffbb538ed617cd8a8116beaf5c777ab429b
변성훈 <sphz3813@gmail.com>
2024-11-30 18:18:29 +0900

    [Silver I] Title: 통나무 건너뛰기, Time: 36 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/11497. 통나무 건너뛰기/통나무 건너뛰기.cc
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int T, N;
+int arr[10005];
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> T;
+
+	while (T--) {
+		int mx = -1;
+
+		cin >> N;
+		for (int i = 0; i < N; i++)
+			cin >> arr[i];
+
+		sort(arr, arr + N); // [2, 4, 5, 7, 9]
+
+		for (int i = 2; i < N; i++) // {2, 5, 9} , {4, 7}로 나누는 것이 최소 간격
+			mx = max(mx, arr[i] - arr[i - 2]);
+		mx = max({ mx, arr[1] - arr[0], arr[N - 1] - arr[N - 2] }); // {2, 5, 9, 7, 4}일 때, {2, 4}, {9, 7} 고려
+
+		cout << mx << "\n";
+	}
+}
