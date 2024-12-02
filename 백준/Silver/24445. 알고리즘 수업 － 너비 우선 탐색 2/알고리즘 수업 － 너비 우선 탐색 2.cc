4a80d6136618d335c02ac72e5e190c6fe4dc30fa
변성훈 <sphz3813@gmail.com>
2024-12-02 21:25:38 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 2, Time: 80 ms, Memory: 8840 KB -BaekjoonHub

--- 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
+++ 백준/Silver/24445. 알고리즘 수업 － 너비 우선 탐색 2/알고리즘 수업 － 너비 우선 탐색 2.cc
@@ -4,23 +4,19 @@
 using namespace std;
 
 int N, M, R, cnt = 1;
-vector<int> v[100001];
 int vis[100001];
+vector<int> v[100001];
 queue<int> q;
 
-int main()
-{
-	ios::sync_with_stdio(0);
-	cin.tie(0);
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
 
 	// freopen("input.txt", "r", stdin);
 
 	cin >> N >> M >> R;
 
-	for (int i = 0, st, en; i < M; i++)
-	{
+	for (int i = 0, st, en; i < M; i++) {
 		cin >> st >> en;
-
 		v[st].push_back(en);
 		v[en].push_back(st);
 	}
@@ -31,22 +27,16 @@ int main()
 	vis[R] = cnt++;
 	q.push(R);
 
-	while (!q.empty())
-	{
+	while (!q.empty()) {
 		auto cur = q.front();
 		q.pop();
 
-
-		for (int i = 0; i < v[cur].size(); i++)
-		{
-			int next = v[cur][i];
-
-			if (!vis[next])
-			{
-				vis[next] = cnt++;
-				q.push(next);
+        for (int nxt : v[cur]) {
+            if (!vis[nxt]) {
+				vis[nxt] = cnt++;
+				q.push(nxt);
 			}
-		}
+        }
 	}
 
 	for (int i = 1; i <= N; i++)
