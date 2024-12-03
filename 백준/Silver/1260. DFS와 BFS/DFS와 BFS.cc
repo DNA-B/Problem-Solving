c86ebe6c0e6cbcaad933bebf96a260846d641137
변성훈 <sphz3813@gmail.com>
2024-12-03 20:52:38 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 5948 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -3,49 +3,39 @@
 using namespace std;
 
 int vu[1001][1001];
-int vis_b[1001];
-int vis_d[1001];
-
+int vis_b[1001], vis_d[1001];
 queue<int> q;
 stack<int> st;
+int n, m, v;
 
-int n, m;
-
-void dfs(int v)
-{
+void dfs(int v) {
     if (vis_d[v] == 1)
         return;
 
     cout << v << " ";
     vis_d[v] = 1;
 
-    for (int i = 1; i < 1001; i++)
-    {
+    for (int i = 1; i < 1001; i++) {
         if (vu[v][i] == 1 && vis_d[i] == 0)
             dfs(i);
     }
 }
 
 
-void bfs(int s)
-{
+void bfs(int s) {
     int nxt = -1;
-
     q.push(s);
 
-    while (!q.empty())
-    {
+    while (!q.empty()) {
         auto cur = q.front();
         q.pop();
 
         cout << cur << " ";
 
-        for (int i = 1; i < 1001; i++)
-        {
+        for (int i = 1; i < 1001; i++) {
             nxt = vu[cur][i];
 
-            if (vis_b[i] == 0 && nxt == 1)
-            {
+            if (vis_b[i] == 0 && nxt == 1) {
                 vis_b[i] = 1;
                 q.push(i);
             }
@@ -53,24 +43,18 @@ void bfs(int s)
     }
 }
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    int n, m, v;
-    int s, e;
+int main() {
+    cin.tie(0)->ios::sync_with_stdio(0);
 
     cin >> n >> m >> v;
-
-    for (int i = 0; i < m; i++)
-    {
+    
+    int s, e;
+    for (int i = 0; i < m; i++) {
         cin >> s >> e;
         vu[s][e] = vu[e][s] = 1;
     }
 
     dfs(v);
-
     cout << "\n";
 
     vis_b[v] = 1;
