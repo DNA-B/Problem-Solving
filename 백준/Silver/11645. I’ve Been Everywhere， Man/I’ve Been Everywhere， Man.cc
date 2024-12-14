9e67b4086ab171bf63e514e48e8f3818fcf6fee4
변성훈 <sphz3813@gmail.com>
2024-12-14 15:15:02 +0900

    [Silver V] Title: I’ve Been Everywhere, Man, Time: 0 ms, Memory: 2160 KB -BaekjoonHub

---
+++ 백준/Silver/11645. I’ve Been Everywhere， Man/I’ve Been Everywhere， Man.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int T, N;
+string city;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> T;
+	while (T--) {
+		unordered_set<string> us;
+
+		cin >> N;
+		while (N--) {
+			cin >> city;
+			us.insert(city);
+		}
+
+		cout << us.size() << '\n';
+	}
+}
