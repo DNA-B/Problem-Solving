bd43af16cc0e238c2ae927cbaf4a8fe2a1ac0abb
변성훈 <sphz3813@gmail.com>
2024-12-15 14:39:29 +0900

    [Silver II] Title: 침략자 진아, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15812. 침략자 진아/침략자 진아.cc
+++ 백준/Silver/15812. 침략자 진아/침략자 진아.cc
@@ -15,45 +15,8 @@ using namespace std;
 //======= VARIABLEs =======//
 int N, M, mn = INT_MAX;
 char board[25][25];
-int dx[] = { 1, -1, 0, 0 };
-int dy[] = { 0, 0, 1, -1 };
-vector<pair<int, int>> house;
+vector<pair<int, int>> house, zero;
 //=========================//
-
-int bfs(int x1, int y1, int x2, int y2) {
-	int vis[25][25];
-	queue<pair<int, int>> q;
-
-	for (int i = 0; i < N; i++)
-		fill(vis[i], vis[i] + M, -1);
-
-	vis[x1][y1] = vis[x2][y2] = 0;
-	q.push({ x1, y1 });
-	q.push({ x2, y2 });
-
-	while (!q.empty()) {
-		auto [x, y] = q.front(); q.pop();
-
-		for (int dir = 0; dir < 4; dir++) {
-			int nx = x + dx[dir];
-			int ny = y + dy[dir];
-
-			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-				continue;
-			if (vis[nx][ny] != -1)
-				continue;
-
-			vis[nx][ny] = vis[x][y] + 1;
-			q.push({ nx, ny });
-		}
-	}
-
-	int mx = -1;
-	for (auto [x, y] : house)
-		mx = max(mx, vis[x][y]);
-
-	return mx;
-}
 
 int main() {
 	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
@@ -64,24 +27,23 @@ int main() {
 	for (int i = 0; i < N; i++) {
 		for (int j = 0; j < M; j++) {
 			cin >> board[i][j];
+
 			if (board[i][j] == '1')
 				house.push_back({ i, j });
+			else
+				zero.push_back({ i, j });
 		}
 	}
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++) {
-			if (board[i][j] == '1')
+	for (auto z1 : zero) {
+		for (auto z2 : zero) {
+			if (z1 == z2)
 				continue;
-			for (int k = 0; k < N; k++) {
-				for (int l = 0; l < M; l++) {
-					if (board[k][l] == '1')
-						continue;
-					if (k == i && l == j)
-						continue;
-					mn = min(mn, bfs(i, j, k, l));
-				}
-			}
+
+			int mx = -1;
+			for (auto h : house)
+				mx = max(mx, min(abs(z1.X - h.X) + abs(z1.Y - h.Y), abs(z2.X - h.X) + abs(z2.Y - h.Y)));
+			mn = min(mn, mx);
 		}
 	}
 
