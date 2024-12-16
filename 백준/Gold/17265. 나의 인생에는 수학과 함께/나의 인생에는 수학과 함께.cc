22e9fcc4c56b6de275d82d8d4c546ce1e13d3d51
변성훈 <sphz3813@gmail.com>
2024-12-16 22:00:35 +0900

    [Gold V] Title: 나의 인생에는 수학과 함께, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.cc
+++ 백준/Gold/17265. 나의 인생에는 수학과 함께/나의 인생에는 수학과 함께.cc
@@ -29,34 +29,30 @@ int calc(char op, int a, int b) { // 연산자, 피연산자1, 피연산자2
 	}
 }
 
-void dig(int x, int y) {
+void solve(int x, int y) {
+	bool is_digit = ('0' <= board[x][y] && board[x][y] <= '5');
 	int mx = INT_MIN, mn = INT_MAX;
 
 	if (x - 1 > 0) {
-		mx = max(mx, calc(board[x - 1][y], mem_mx[x - 1][y], board[x][y] - '0'));
-		mn = min(mn, calc(board[x - 1][y], mem_mn[x - 1][y], board[x][y] - '0'));
+		if (is_digit) {
+			mx = max(mx, calc(board[x - 1][y], mem_mx[x - 1][y], board[x][y] - '0'));
+			mn = min(mn, calc(board[x - 1][y], mem_mn[x - 1][y], board[x][y] - '0'));
+		}
+		else {
+			mx = max(mx, mem_mx[x - 1][y]);
+			mn = min(mn, mem_mn[x - 1][y]);
+		}
 	}
 	if (y - 1 > 0) {
-		mx = max(mx, calc(board[x][y - 1], mem_mx[x][y - 1], board[x][y] - '0'));
-		mn = min(mn, calc(board[x][y - 1], mem_mn[x][y - 1], board[x][y] - '0'));
+		if (is_digit) {
+			mx = max(mx, calc(board[x][y - 1], mem_mx[x][y - 1], board[x][y] - '0'));
+			mn = min(mn, calc(board[x][y - 1], mem_mn[x][y - 1], board[x][y] - '0'));
+		}
+		else {
+			mx = max(mx, mem_mx[x][y - 1]);
+			mn = min(mn, mem_mn[x][y - 1]);
+		}
 	}
-
-	mem_mx[x][y] = mx;
-	mem_mn[x][y] = mn;
-}
-
-void op(int x, int y) {
-	int mx = INT_MIN, mn = INT_MAX;
-
-	if (x - 1 > 0) {
-		mx = max(mx, mem_mx[x - 1][y]);
-		mn = min(mn, mem_mn[x - 1][y]);
-	}
-	if (y - 1 > 0) {
-		mx = max(mx, mem_mx[x][y - 1]);
-		mn = min(mn, mem_mn[x][y - 1]);
-	}
-
 	mem_mx[x][y] = mx;
 	mem_mn[x][y] = mn;
 }
@@ -68,19 +64,20 @@ int main() {
 	cin >> N;
 
 	for (int i = 1; i <= N; i++) {
-		fill(mem_mx[i], mem_mx[i] + N, INT_MIN);
-		fill(mem_mn[i], mem_mn[i] + N, INT_MAX);
 		for (int j = 1; j <= N; j++)
 			cin >> board[i][j];
+		fill(mem_mx[i] + 1, mem_mx[i] + (N + 1), INT_MIN);
+		fill(mem_mn[i] + 1, mem_mn[i] + (N + 1), INT_MAX);
 	}
 
 	for (int i = 1; i <= N; i++) {
 		for (int j = 1; j <= N; j++) {
-			if (i == 1 && j == 1) mem_mx[i][j] = mem_mn[i][j] = board[i][j] - '0';
-			else if ('0' <= board[i][j] && board[i][j] <= '5') dig(i, j);
-			else op(i, j);
+			if (i == 1 && j == 1)
+				mem_mx[i][j] = mem_mn[i][j] = board[i][j] - '0';
+			else
+				solve(i, j);
 		}
 	}
-
+    
 	cout << mem_mx[N][N] << ' ' << mem_mn[N][N];
 }
