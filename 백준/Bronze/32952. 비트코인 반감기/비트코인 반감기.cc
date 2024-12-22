665f64369b3230335be067942177c99e7fe4281d
변성훈 <sphz3813@gmail.com>
2024-12-22 23:14:27 +0900

    [Bronze II] Title: 비트코인 반감기, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/32952. 비트코인 반감기/비트코인 반감기.cc
@@ -0,0 +1,25 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_map>
+#include <unordered_set>
+using namespace std;
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+ll R, K, M;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
+
+	cin >> R >> K >> M;
+	cout << R / (ll)pow(2, M / K);
+}
