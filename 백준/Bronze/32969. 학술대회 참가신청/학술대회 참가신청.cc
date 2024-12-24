291f5dc74c0ed42feb98d61b862becd082ff3008
변성훈 <sphz3813@gmail.com>
2024-12-24 23:42:08 +0900

    [Bronze III] Title: 학술대회 참가신청, Time: 0 ms, Memory: 2032 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.cc
+++ 백준/Bronze/32969. 학술대회 참가신청/학술대회 참가신청.cc
@@ -2,7 +2,12 @@
 #include <bits/stdc++.h>
 using namespace std;
 //========= MACRO =========//
-
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
 //=========================//
 
 //======= VARIABLEs =======//
@@ -16,6 +21,13 @@ unordered_map<string, string> um = {
 
 int main() {
 	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
-	//freopen("input.txt", "r", stdin);
-	cin >> s; cout << um[s];
+	// freopen("input.txt", "r", stdin);
+
+	getline(cin, s);
+	for (auto [word, track] : um) {
+		if (s.find(word) != -1) {
+			cout << track;
+			break;
+		}
+	}
 }
