a09e5748dba4597f56a29a85bcba25604f2da8af
변성훈 <sphz3813@gmail.com>
2024-12-25 18:06:08 +0900

    [Gold III] Title: 벽 부수고 이동하기, Time: 76 ms, Memory: 12692 KB -BaekjoonHub

--- 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
+++ 백준/Gold/2206. 벽 부수고 이동하기/벽 부수고 이동하기.cc
@@ -1,71 +1,68 @@
+#define _CRT_SECURE_NO_WARNINGS
 #include <bits/stdc++.h>
 
 using namespace std;
 
+/******** MACRO ********/
+#define PI 3.1415926535897932
+#define MOD 1000000007
+#define INT_INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+/***********************/
+
+/****** VARIABLEs ******/
 string board[1000];
 int vis[1000][1000][2]; // x, y, 벽 부쉈는지 여부
-
 int n, m;
 int dx[4] = { -1, 1, 0, 0 }; // 상하좌우
 int dy[4] = { 0, 0, -1 ,1 };
+/***********************/
 
-int bfs()
-{
-    queue<pair<pair<int, int>, pair<int, int>>> q;
+int bfs() {
+	queue<tuple<int, int, bool>> q;
+	q.push({ 0, 0, false }); // {x,y}, {벽 부순 여부, 거리}
+	vis[0][0][0] = 1;
 
-    q.push({ { 0,0 }, {0,1} }); // {x,y}, {벽 부순 여부, 거리}
-    vis[0][0][0] = 1;
+	while (!q.empty()) {
+		auto [x, y, is_crash] = q.front();
+		q.pop();
 
-    while (!q.empty())
-    {
-        auto cur = q.front();
-        int is_crash = cur.second.first;
-        int cnt = cur.second.second;
+		if (x == n - 1 && y == m - 1) // 도착
+			return vis[x][y][is_crash];
 
-        q.pop();
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = x + dx[dir]; // 방향 설정
+			int ny = y + dy[dir];
 
-        if ((cur.first.first == n - 1) && (cur.first.second == m - 1)) // 도착
-            return cnt;
+			if (nx < 0 || nx >= n || ny >= m || ny < 0)
+				continue; // 범위 밖일 경우
+			if (vis[nx][ny][is_crash])
+				continue;
 
-        for (int dir = 0; dir < 4; dir++)
-        {
-            int nx = cur.first.first + dx[dir]; // 방향 설정
-            int ny = cur.first.second + dy[dir];
+			if (board[nx][ny] == '1' && is_crash == 0) { // 벽 있는데 안 부쉈으면
+				vis[nx][ny][!is_crash] = vis[x][y][is_crash] + 1;
+				q.push({ nx, ny, !is_crash });
+			}
+			else if (board[nx][ny] == '0') { // 벽 없으면
+				vis[nx][ny][is_crash] = vis[x][y][is_crash] + 1;
+				q.push({ nx, ny, is_crash });
+			}
+		}
+	}
 
-            if (nx < 0 || nx >= n || ny >= m || ny < 0)
-                continue; // 범위 밖일 경우
-
-            if (board[nx][ny] == '1' && is_crash == 0) // 벽 있는데 안 부쉈으면
-            {
-                vis[nx][ny][is_crash + 1] = 1;
-                q.push({ {nx,ny},{is_crash + 1, cnt + 1} });
-            }
-            else if (board[nx][ny] == '0' && vis[nx][ny][is_crash] == 0) // 벽 없으면
-            {
-                vis[nx][ny][is_crash] = 1;
-                q.push({ { nx,ny }, {is_crash, cnt + 1} });
-            }
-        }
-    }
-    
-    // 도착 못함
-    return -1;
+	return -1; // 도착 못함
 }
 
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+	// freopen("input.txt", "r", stdin);
 
-int main()
-{
-    ios::sync_with_stdio(0);
-    cin.tie(0);
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) cin >> board[i];
+	cout << bfs();
 
-    cin >> n >> m;
-
-    for (int i = 0; i < n; i++)
-    {
-        cin >> board[i];
-    }
-
-    cout << bfs();
-
-    return 0;
+	return 0;
 }
