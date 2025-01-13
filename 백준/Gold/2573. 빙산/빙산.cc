0f6b10c3571d554d5c47592d5f9c7b4a70b0f3b7
변성훈 <sphz3813@gmail.com>
2025-01-13 21:08:37 +0900

    [Gold IV] Title: 빙산, Time: 48 ms, Memory: 2752 KB -BaekjoonHub

--- 백준/Gold/2573. 빙산/빙산.cc
+++ 백준/Gold/2573. 빙산/빙산.cc
@@ -12,40 +12,18 @@ using namespace std;
 //=========================//
 
 //======= VARIABLEs =======//
-int N, M, cnt = 0;
+int N, M, cnt = 0, ice = 0;
 int board[305][305], vis[305][305];
 int dx[] = { 1, -1, 0, 0 };
 int dy[] = { 0, 0, 1, -1 };
 queue<pair<int, int>> q;
 //=========================//
 
-bool is_discrete() {
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++)
-			if (board[i][j] && !vis[i][j])
-				return true;
-	}
-	return false;
-}
-
-void melt(int x, int y) {
-	for (int dir = 0; dir < 4; dir++) {
-		int nx = x + dx[dir];
-		int ny = y + dy[dir];
-
-		if (nx < 0 || nx >= N || ny < 0 || ny >= M)
-			continue;
-		if (vis[nx][ny] || board[nx][ny] != 0)
-			continue;
-
-		board[x][y] = max(0, board[x][y] - 1);
-	}
-}
-
-void bfs() {
+bool bfs() {
+	int touch = 0, melt = 0;
 	while (!q.empty()) {
 		auto [x, y] = q.front(); q.pop();
-		melt(x, y);
+		touch++;
 
 		for (int dir = 0; dir < 4; dir++) {
 			int nx = x + dx[dir];
@@ -53,13 +31,27 @@ void bfs() {
 
 			if (nx < 0 || nx >= N || ny < 0 || ny >= M)
 				continue;
-			if (vis[nx][ny] || !board[nx][ny])
+			if (vis[nx][ny])
 				continue;
+			if (!board[nx][ny]) {
+				board[x][y] = max(0, board[x][y] - 1);
+				continue;
+			}
 
 			vis[nx][ny] = 1;
 			q.push({ nx, ny });
 		}
+
+		if (!board[x][y])
+			melt++;
 	}
+
+	if (touch == ice) {
+		ice -= melt;
+		return true;
+	}
+
+	return false;
 }
 
 bool solve() {
@@ -69,13 +61,12 @@ bool solve() {
 			if (board[i][j] && !vis[i][j]) {
 				q.push({ i, j });
 				vis[i][j] = 1;
-				bfs();
-				return true;
+				return bfs();
 			}
 		}
 	}
 
-	cnt = 0;
+	cnt = 0; // 모든 빙산 녹았다면 0 출력
 	return false;
 }
 
@@ -89,14 +80,15 @@ int main() {
 	cin >> N >> M;
 
 	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < M; j++)
+		for (int j = 0; j < M; j++) {
 			cin >> board[i][j];
+			if (board[i][j]) ice++;
+		}
 	}
 
 	while (solve()) {
-		if (is_discrete()) break;
-		else cnt++;
 		memset(vis, 0, sizeof(vis));
+		cnt++;
 	}
 
 	cout << cnt;
