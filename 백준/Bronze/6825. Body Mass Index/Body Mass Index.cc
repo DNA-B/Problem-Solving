6ea3394a69da050b237912872114ddc7319da40e
변성훈 <sphz3813@gmail.com>
2025-01-21 19:23:46 +0900

    [Bronze IV] Title: Body Mass Index, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/6825. Body Mass Index/Body Mass Index.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+double w, h, BMI;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> w >> h;
+	BMI = w / (h * h);
+
+	if (BMI > 25)
+		cout << "Overweight";
+	else if (BMI < 18.5)
+		cout << "Underweight";
+	else
+		cout << "Normal weight";
+}
