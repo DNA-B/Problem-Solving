4f11174fd0dbbf8f611f4bac43f4c0f8c13596f6
변성훈 <sphz3813@gmail.com>
2025-01-26 00:07:27 +0900

    [Bronze V] Title: Конвейер, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/28938. Конвейер/Конвейер.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int n, x, cur = 0;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> n;
+
+	while (n--) {
+		cin >> x;
+		cur += x;
+	}
+
+	if (cur < 0)
+		cout << "Left";
+	else if (cur > 0)
+		cout << "Right";
+	else
+		cout << "Stay";
+}
