aff17f13a9bf702fa20f2ef0f3ec34f06a56b356
변성훈 <sphz3813@gmail.com>
2025-02-02 22:19:25 +0900

    [Bronze III] Title: N M 찍기, Time: 80 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18883. N M 찍기/N M 찍기.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, M;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N >> M;
+
+	int cnt = 1;
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < M; j++) {
+			cout << cnt++;
+			if (j == M - 1) continue;
+			else cout << ' ';
+		}
+		cout << '\n';
+	}
+}
