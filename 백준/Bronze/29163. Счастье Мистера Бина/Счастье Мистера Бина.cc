95f14bfce96495d3d47887211089657cee6177a0
변성훈 <sphz3813@gmail.com>
2025-02-06 21:43:16 +0900

    [Bronze V] Title: Счастье Мистера Бина, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/29163. Счастье Мистера Бина/Счастье Мистера Бина.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, odd=0, even=0;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	int x;
+	while (N--) {
+		cin >> x;
+		odd += (x & 1);
+		even += !(x & 1);
+	}
+
+	cout << (odd >= even ? "Sad" : "Happy");
+}
