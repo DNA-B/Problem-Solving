743a96a31f6c9e6605ebf253c4244c48fbaee40c
변성훈 <sphz3813@gmail.com>
2025-02-16 18:25:24 +0900

    [Silver III] Title: 수 이어 쓰기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/1515. 수 이어 쓰기/수 이어 쓰기.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+string S;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> S;
+
+	int cur = 0, cnt = 0;
+	while (cur < S.size()) {
+		cnt++;
+		for (char ch : to_string(cnt))
+			if (ch == S[cur]) cur++;
+	}
+
+	cout << cnt;
+}
