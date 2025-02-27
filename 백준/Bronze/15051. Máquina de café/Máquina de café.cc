08e7b18fadcce3da6d3b94e33494e37c79fd1bed
변성훈 <sphz3813@gmail.com>
2025-02-27 22:18:44 +0900

    [Bronze IV] Title: Máquina de café, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15051. Máquina de café/Máquina de café.cc
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int A1, A2, A3;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> A1 >> A2 >> A3;
+	cout << min({ A2 * 2 + A3 * 4, A1 * 2 + A3 * 2, A1 * 4 + A2 * 2 });
+}
