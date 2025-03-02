149daa233eadb6f8033d6f98881e31e23c702760
변성훈 <sphz3813@gmail.com>
2025-03-02 22:25:56 +0900

    [Bronze IV] Title: 鉛筆 (Pencils), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15474. 鉛筆 （Pencils）/鉛筆 （Pencils）.cc
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, A, B, C, D;
+int tmp1, tmp2;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N >> A >> B >> C >> D;
+	tmp1 = (N % A == 0 ? (N / A) * B : ((N / A) + 1) * B);
+	tmp2 = (N % C == 0 ? (N / C) * D : ((N / C) + 1) * D);
+	cout << min(tmp1, tmp2);
+}
