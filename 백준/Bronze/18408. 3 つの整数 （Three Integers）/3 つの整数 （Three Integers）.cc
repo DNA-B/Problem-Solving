ee4d8349c76c106bc1b5a1afbb7dbafd7962bcff
변성훈 <sphz3813@gmail.com>
2025-03-06 21:03:46 +0900

    [Bronze IV] Title: 3 つの整数 (Three Integers), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18408. 3 つの整数 （Three Integers）/3 つの整数 （Three Integers）.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int x, one = 0, two = 0;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	for (int i = 0; i < 3; i++) {
+		cin >> x;
+		one += (x == 1);
+		two += (x == 2);
+	}
+
+	cout << (one > two ? 1 : 2);
+}
