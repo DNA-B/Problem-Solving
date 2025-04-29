3c985fa2a700d5b3f47200d7e38d221e17498b71
변성훈 <sphz3813@gmail.com>
2025-04-29 10:49:20 +0900

    [Gold V] Title: 돌 게임 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.cc
@@ -0,0 +1,30 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+ll N;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	if (N % 7 == 0 || N % 7 == 2) cout << "CY";
+	else cout << "SK";
+}
