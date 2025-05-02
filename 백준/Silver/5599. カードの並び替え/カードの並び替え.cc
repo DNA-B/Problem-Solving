a24ceab4fa3797f92bfd8e6d4c12878392a0c5bb
변성훈 <sphz3813@gmail.com>
2025-05-02 17:22:41 +0900

    [Silver V] Title: カードの並び替え, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/5599. カードの並び替え/カードの並び替え.cc
@@ -0,0 +1,58 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, M;
+int arr[205];
+//=========================//
+
+void rf_shuffle() {
+	int tmp[205];
+	for (int i = 0; i < 2 * N; i++) {
+		if (i % 2 == 0) tmp[i] = arr[i / 2];
+		else tmp[i] = arr[N + (i / 2)];
+	}
+
+	for (int i = 0; i < 2 * N; i++)
+		arr[i] = tmp[i];
+}
+
+void k_shuffle(int k) {
+	rotate(arr, arr + k, arr + 2 * N);
+}
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	for (int i = 0; i < 2 * N; i++)
+		arr[i] = i + 1;
+
+	cin >> M;
+
+	int x;
+	while (M--) {
+		cin >> x;
+		if (!x) rf_shuffle();
+		else k_shuffle(x);
+	}
+
+	for (int i = 0; i < 2 * N; i++)
+		cout << arr[i] << '\n';
+}
