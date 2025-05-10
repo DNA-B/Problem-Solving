ec219e5c07333f1f074db5b305792eed3189c4d3
변성훈 <sphz3813@gmail.com>
2025-05-10 16:47:34 +0900

    [Bronze IV] Title: Cutting Corners, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/20215. Cutting Corners/Cutting Corners.cc
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int w, h;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> w >> h;
+	cout << setprecision(9);
+	cout << (w + h) - sqrt((w * w) + (h * h));
+}
