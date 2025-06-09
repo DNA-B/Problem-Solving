852816e3644834328548a6f7342ebe88057268b7
변성훈 <sphz3813@gmail.com>
2025-06-09 14:45:00 +0900

    [level 0] Title: 문자 리스트를 문자열로 변환하기, Time: 0.02 ms, Memory: 4.16 MB -BaekjoonHub

---
+++ 프로그래머스/0/181941. 문자 리스트를 문자열로 변환하기/문자 리스트를 문자열로 변환하기.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(vector<string> arr) {
+    string answer = "";
+    for(string s : arr) answer += s;
+    return answer;
+}
