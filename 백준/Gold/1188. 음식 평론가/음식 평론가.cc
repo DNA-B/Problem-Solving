69143382ba6b0c3fd18579a5edaefc32f685f601
변성훈 <sphz3813@gmail.com>
2025-06-17 19:07:09 +0900

    [Gold IV] Title: 음식 평론가, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Gold/1188. 음식 평론가/음식 평론가.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, M;
+//=========================//
+
+int gcd(int a, int b) {
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N >> M;
+	cout << M - gcd(N, M);
+}
