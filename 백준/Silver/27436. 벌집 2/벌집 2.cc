51bda239162a9bede5b382376ff4a2b1848b03f5
변성훈 <sphz3813@gmail.com>
2025-06-21 20:56:05 +0900

    [Silver III] Title: 벌집 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/27436. 벌집 2/벌집 2.cc
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+#include <unordered_set>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+ll  N;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	ll lo = 1, hi = 2e9;
+	while (lo < hi) {
+		ll mid = lo + (hi - lo) / 2;
+		ll room = 3 * mid * (mid - 1) + 1;
+
+		if (room < N)
+			lo = mid + 1;
+		else
+			hi = mid;
+	}
+
+	cout << hi;
+}
