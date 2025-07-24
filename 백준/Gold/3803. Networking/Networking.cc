b5069a5ebabdd6f6a7ec9b7a2ab2640f0e046dae
변성훈 <sphz3813@gmail.com>
2025-07-24 21:09:27 +0900

    [Gold IV] Title: Networking, Time: 4 ms, Memory: 2156 KB -BaekjoonHub

---
+++ 백준/Gold/3803. Networking/Networking.cc
@@ -0,0 +1,76 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int P, R;
+vector<int> p(55, -1);
+vector<tuple<int, int, int>> adj;
+//=========================//
+
+int uf_find(int x) {
+	if (p[x] < 0)
+		return x;
+	return p[x] = uf_find(p[x]);
+}
+
+bool uf_union(int a, int b) {
+	a = uf_find(a);
+	b = uf_find(b);
+
+	if (a == b)
+		return false;
+
+	if (p[a] == p[b])
+		p[a]--;
+	if (p[a] < p[b])
+		p[b] = a;
+	else
+		p[a] = b;
+
+	return true;
+}
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	while (cin >> P >> R) {
+		fill(p.begin(), p.end(), -1);
+		adj.clear();
+
+		int u, v, c;
+		for (int i = 0; i < R; i++) {
+			cin >> u >> v >> c;
+			adj.push_back({ c, u, v });
+		}
+
+		sort(adj.begin(), adj.end());
+
+		int cost = 0, cnt = 0;
+		for (auto [c, u, v] : adj) {
+			if (!uf_union(u, v))
+				continue;
+
+			cost += c;
+			cnt++;
+
+			if (cnt == P - 1)
+				break;
+		}
+
+		cout << cost << '\n';
+	}
+}
