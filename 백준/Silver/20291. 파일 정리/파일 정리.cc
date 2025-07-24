e56c1d574a3fd9d928aea7adcb517b99edec0937
변성훈 <sphz3813@gmail.com>
2025-07-24 21:11:43 +0900

    [Silver III] Title: 파일 정리, Time: 96 ms, Memory: 11004 KB -BaekjoonHub

---
+++ 백준/Silver/20291. 파일 정리/파일 정리.cc
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+map<string, int> m;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	string fname;
+	while (N--) {
+		cin >> fname;
+		string ext = fname.substr(fname.find_last_of('.') + 1);
+		m[ext]++;
+	}
+
+	for (auto [name, cnt] : m)
+		cout << name << " " << cnt << "\n";
+}
