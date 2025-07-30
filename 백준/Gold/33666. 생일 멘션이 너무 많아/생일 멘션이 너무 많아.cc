1822e67f8d18f9724db09d7a4b6b48d52a27dbfa
변성훈 <sphz3813@gmail.com>
2025-07-31 04:06:16 +0900

    [Gold V] Title: 생일 멘션이 너무 많아, Time: 196 ms, Memory: 13740 KB -BaekjoonHub

---
+++ 백준/Gold/33666. 생일 멘션이 너무 많아/생일 멘션이 너무 많아.cc
@@ -0,0 +1,63 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, M;
+int imos[1000005];
+ll mentions[1000005];
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N >> M;
+
+	int cnt = 0;
+	ll total = 0, am = 0;
+	for (int i = 1; i <= N; i++) {
+		cin >> mentions[i];
+		if (mentions[i] >= 2) {
+			total += mentions[i];
+			cnt++;
+		}
+	}
+
+	am = (cnt == 0) ? 0 : total / cnt;
+
+	for (int i = 1; i <= N; i++) {
+		if (mentions[i] <= am) {
+			if (mentions[i] > M) {
+				cout << -1;
+				exit(0);
+			}
+			else {
+				imos[1]++;
+				imos[mentions[i] + 1]--;
+			}
+		}
+		else {
+			imos[1]++;
+			imos[2]--;
+		}
+	}
+
+
+	for (int i = 1; i <= M; i++) {
+		imos[i] += imos[i - 1];
+		cout << imos[i] << ' ';
+	}
+}
