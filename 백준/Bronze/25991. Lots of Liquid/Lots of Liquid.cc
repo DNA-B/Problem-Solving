8973bf10f0f0b092cb581b84e6e91a8f5db1ae77
변성훈 <sphz3813@gmail.com>
2025-07-31 23:06:23 +0900

    [Bronze IV] Title: Lots of Liquid, Time: 44 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/25991. Lots of Liquid/Lots of Liquid.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+double side, total;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++) {
+		cin >> side;
+		total += pow(side, 3);
+	}
+
+	// 정밀도 15자리로 출력
+	cout << fixed << setprecision(15) << pow(total, 1.0 / 3.0);
+}
