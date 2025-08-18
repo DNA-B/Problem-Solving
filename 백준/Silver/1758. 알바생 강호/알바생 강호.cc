36718810ad76c032f88d069e508217897b6a5c8f
변성훈 <sphz3813@gmail.com>
2025-08-18 22:20:39 +0900

    [Silver IV] Title: 알바생 강호, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.cc
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+int arr[100005];
+ll total;
+//=========================//
+
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	sort(arr, arr + N, greater<>());
+
+	for (int i = 0; i < N; i++)
+		total += max(arr[i] - i, 0);
+
+	cout << total;
+}
