fdb6102b02821085eab93924c750ad2131d26655
변성훈 <sphz3813@gmail.com>
2025-08-26 18:22:58 +0900

    [Bronze IV] Title: Multiple Choice, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/6784. Multiple Choice/Multiple Choice.cc
@@ -0,0 +1,39 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+char arr[10005];
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> arr[i];
+
+	char x;
+	int res = 0;
+	for (int i = 0; i < N; i++) {
+		cin >> x;
+		res += (arr[i] == x);
+	}
+
+	cout << res;
+}
