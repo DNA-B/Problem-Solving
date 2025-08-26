cabf4942d0fb6ebe8ae28ea6287c158c352bd9e7
변성훈 <sphz3813@gmail.com>
2025-08-26 18:20:07 +0900

    [Bronze IV] Title: 다면체, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10569. 다면체/다면체.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int T, V, E;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> T;
+
+	while (T--) { // (꼭짓점의 수) - (모서리의 수) + (면의 수) = 2
+		cin >> V >> E;
+		cout << 2 - (V - E) << '\n';
+	}
+}
