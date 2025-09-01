19302179587cd1de32cb5d25c7fc1b3a639bdb3d
변성훈 <sphz3813@gmail.com>
2025-09-01 19:13:43 +0900

    [Bronze IV] Title: Piece of Cake!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/17874. Piece of Cake！/Piece of Cake！.cc
@@ -0,0 +1,27 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, H, V;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N >> H >> V;
+	cout << max(N - H, H) * max(N - V, V) * 4;
+}
