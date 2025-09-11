ae4c1513e940b5e2b6133b99f4910c16b2188254
변성훈 <sphz3813@gmail.com>
2025-09-11 20:56:04 +0900

    [Bronze IV] Title: Quality-Adjusted Life-Year, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/22279. Quality－Adjusted Life－Year/Quality－Adjusted Life－Year.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+double q, y, QALY = 0;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	while (N--) {
+		cin >> q >> y;
+		QALY += q * y;
+	}
+
+	cout << fixed << setprecision(5);
+	cout << QALY;
+}
