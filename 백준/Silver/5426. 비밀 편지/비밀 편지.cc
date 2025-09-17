ee3aa1284e381fe5f03f90b79902bd3599bd256f
변성훈 <sphz3813@gmail.com>
2025-09-17 17:24:30 +0900

    [Silver V] Title: 비밀 편지, Time: 8 ms, Memory: 2184 KB -BaekjoonHub

---
+++ 백준/Silver/5426. 비밀 편지/비밀 편지.cc
@@ -0,0 +1,45 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int T;
+string message;
+char mtoa[105][105];
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> T;
+
+	while (T--) {
+		cin >> message;
+
+		int len = sqrt(message.length());
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < len; j++)
+				mtoa[i][j] = message[i * len + j];
+		}
+
+		for (int i = 0; i < len; i++) {
+			for (int j = 0; j < len; j++)
+				cout << mtoa[j][len - i - 1];
+		}
+
+		cout << '\n';
+	}
+}
