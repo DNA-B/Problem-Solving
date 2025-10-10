59ec2c45903936d85983cffda4b950d06482536c
변성훈 <sphz3813@gmail.com>
2025-10-10 21:12:30 +0900

    [Bronze IV] Title: Bicycle, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20233. Bicycle/Bicycle.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int a, x, b, y, T;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> a >> x >> b >> y >> T;
+
+	int A = a + 21 * (x * max(T - 30, 0));
+	int B = b + 21 * (y * max(T - 45, 0));
+
+	cout << A << ' ' << B;
+}
