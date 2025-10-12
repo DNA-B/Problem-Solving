cc10a81e8b6eccc0d5388f73fe152a5f8d3c0f10
변성훈 <sphz3813@gmail.com>
2025-10-12 23:11:51 +0900

    [Bronze IV] Title: Another Eruption, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

---
+++ 백준/Bronze/21335. Another Eruption/Another Eruption.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+double a;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> a;
+	cout << fixed << setprecision(10);
+	a /= PI;
+	a = sqrt(a);
+	a = 2 * PI * a;
+	cout << a;
+}
