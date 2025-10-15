156a0356ba1aeccf62f39900eb87bb64895487d9
변성훈 <sphz3813@gmail.com>
2025-10-16 07:36:51 +0900

    [Bronze II] Title: 꿍 가라사대, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11094. 꿍 가라사대/꿍 가라사대.cc
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+string S;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+	cin.ignore();
+
+	while (N--) {
+		getline(cin, S);
+
+		if (S.length() < 10)
+			continue;
+		if (S.substr(0, 10) == "Simon says")
+			cout << S.substr(10) << '\n';
+	}
+}
