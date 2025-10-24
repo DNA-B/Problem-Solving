940ed002d58938dae1f32195b302d475274cf32d
변성훈 <sphz3813@gmail.com>
2025-10-24 16:55:33 +0900

    [Silver III] Title: N결수, Time: 1412 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/27965. N결수/N결수.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <bits/stdc++.h>
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+ll N, K;
+string S = "0";
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N >> K;
+
+	for (int i = 1; i <= N; i++) {
+		S += to_string(i);
+		ll tmp = stoll(S);
+		tmp %= K;
+		S = to_string(tmp);
+	}
+
+	cout << S;
+}
