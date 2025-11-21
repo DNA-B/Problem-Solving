a6d760324845b2c04fd203b6a4bec50c61e5fc76
변성훈 <sphz3813@gmail.com>
2025-11-21 14:40:31 +0900

    [Bronze III] Title: 최댓값, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/2566. 최댓값/최댓값.cc
+++ 백준/Bronze/2566. 최댓값/최댓값.cc
@@ -2,30 +2,26 @@
 #include<stdio.h>
 
 
-int main()
-{
+int main() {
 	int a[9][9];
 	int max = -1;
 	int m, n;
 
-	for (int i = 0; i < 9; i++)
-	{
+	for (int i = 0; i < 9; i++) {
 		for (int j = 0; j < 9; j++)
 			scanf("%d", &a[i][j]);
 	}
 
-	for (int i = 0; i < 9; i++)
-	{
-		for (int j = 0; j < 9; j++)
-		{
-			if (max < a[i][j])
-			{
+	for (int i = 0; i < 9; i++) {
+		for (int j = 0; j < 9; j++) {
+			if (max < a[i][j]) {
 				max = a[i][j];
 				m = i + 1;
 				n = j + 1;
 			}
 		}
 	}
+    
 	printf("%d\n", max);
 	printf("%d %d", m, n);
 }
