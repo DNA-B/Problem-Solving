3f84fac04dcf459301952c9361a1d9eb90cce24c
변성훈 <sphz3813@gmail.com>
2025-11-26 19:27:23 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.cc
@@ -0,0 +1,85 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int money, JH, SM;
+int MD[15];
+//=========================//
+
+int proc_JH() {
+	int cur = money, stock = 0;
+
+	for (int i = 0; i < 14; i++) {
+		if (cur >= MD[i]) {
+			stock += cur / MD[i];
+			cur -= (cur / MD[i]) * MD[i];
+		}
+	}
+
+	return cur + (stock * MD[13]);
+}
+
+int proc_SM() {
+	int cur = money, stock = 0;
+	int up_cnt = 0, down_cnt = 0;
+
+	for (int i = 1; i < 14; i++) {
+		if (MD[i] > MD[i - 1])
+			up_cnt++;
+		else
+			up_cnt = 0;
+
+		if (MD[i] < MD[i - 1])
+			down_cnt++;
+		else
+			down_cnt = 0;
+
+		if (up_cnt == 3) {
+			cur += stock * MD[i];
+			stock = 0;
+            up_cnt = 0;
+        }
+
+		if (down_cnt == 3) {
+			stock += cur / MD[i];
+			cur -= (cur / MD[i]) * MD[i];
+			down_cnt = 0;
+		}
+	}
+
+	return cur + (stock * MD[13]);
+}
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> money;
+
+	for (int i = 0; i < 14; i++)
+		cin >> MD[i];
+
+	JH = proc_JH();
+	SM = proc_SM();
+
+	if (JH == SM)
+		cout << "SAMESAME";
+	else if (JH > SM)
+		cout << "BNP";
+	else
+		cout << "TIMING";
+}
