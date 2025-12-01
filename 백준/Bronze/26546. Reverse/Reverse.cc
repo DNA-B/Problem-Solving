fcfa1f49c6c3bcc0c92b8f02eba893e786b98cda
변성훈 <sphz3813@gmail.com>
2025-12-01 22:22:12 +0900

    [Bronze IV] Title: Reverse, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26546. Reverse/Reverse.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	while (N--) {
+		string S;
+		int i, j;
+
+		cin >> S >> i >> j;
+		S.erase(i, j - i);
+		cout << S << '\n';
+	}
+}
