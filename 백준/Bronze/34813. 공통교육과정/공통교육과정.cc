4895eb348b3135ba2f5e3af7a05c4aec3343dcdb
변성훈 <sphz3813@gmail.com>
2025-12-05 18:41:46 +0900

    [Bronze V] Title: 공통교육과정, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/34813. 공통교육과정/공통교육과정.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+string lnum;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> lnum;
+
+	switch (lnum[0]) {
+	case 'F':
+		cout << "Foundation";
+		break;
+	case 'C':
+		cout << "Claves";
+		break;
+	case 'V':
+		cout << "Veritas";
+		break;
+	case 'E':
+		cout << "Exploration";
+		break;
+	}
+}
