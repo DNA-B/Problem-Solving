83604fe358a942c49fef31ecdb9e6a2ff87d6739
변성훈 <sphz3813@gmail.com>
2025-12-09 18:08:39 +0900

    [Bronze IV] Title: Сравнение комнат, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27267. Сравнение комнат/Сравнение комнат.cc
@@ -0,0 +1,34 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int A, B, C, D;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> A >> B >> C >> D;
+
+	if (A * B == C * D)
+		cout << 'E';
+	else if (A * B > C * D)
+		cout << 'M';
+	else
+		cout << 'P';
+}
