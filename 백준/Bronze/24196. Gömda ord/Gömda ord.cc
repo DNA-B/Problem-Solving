b9aa49688578ad6e478aceaf93d492a2975dd56f
변성훈 <sphz3813@gmail.com>
2025-12-21 15:51:55 +0900

    [Bronze IV] Title: Gömda ord, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/24196. Gömda ord/Gömda ord.cc
@@ -0,0 +1,42 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+string code;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> code;
+
+	string res = "";
+	int idx = 0;
+	while (idx < code.length()) {
+		if (idx == code.length() - 1) {
+			cout << code[idx];
+			break;
+		}
+		else {
+			cout << code[idx];
+			idx += code[idx] - 'A' + 1;
+		}
+	}
+
+	cout << res;
+}
