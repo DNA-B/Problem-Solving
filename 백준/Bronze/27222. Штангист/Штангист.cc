9909dcf37e7bc98de4f5875bbee3e5bef222a88e
변성훈 <sphz3813@gmail.com>
2025-12-25 04:29:54 +0900

    [Bronze IV] Title: Штангист, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27222. Штангист/Штангист.cc
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, res = 0;
+bool is_trained[1005];
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> is_trained[i];
+
+	int x, y;
+	for (int i = 0; i < N; i++) {
+		cin >> x >> y;
+		if (is_trained[i] && y - x > 0)
+			res += y - x;
+	}
+
+	cout << res;
+}
