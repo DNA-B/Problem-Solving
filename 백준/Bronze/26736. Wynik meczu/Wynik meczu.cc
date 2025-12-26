2999270ea254fea629addccc849d26e58f913240
변성훈 <sphz3813@gmail.com>
2025-12-26 22:27:06 +0900

    [Bronze IV] Title: Wynik meczu, Time: 0 ms, Memory: 2300 KB -BaekjoonHub

---
+++ 백준/Bronze/26736. Wynik meczu/Wynik meczu.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int A = 0, B = 0;
+string S;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> S;
+
+	for (char ch : S) {
+		A += (ch == 'A');
+		B += (ch == 'B');
+	}
+
+	cout << A << " : " << B;
+}
