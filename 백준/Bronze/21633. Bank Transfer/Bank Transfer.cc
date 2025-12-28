937bcf2d4e1581cd5399f8f21a6f61a6f0a03dce
변성훈 <sphz3813@gmail.com>
2025-12-28 18:22:03 +0900

    [Bronze IV] Title: Bank Transfer, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/21633. Bank Transfer/Bank Transfer.cc
@@ -0,0 +1,29 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+double K, charge = 100;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> K;
+	charge = min(2000.0, max(charge, 25 + ((K / 25) * 0.25)));
+	cout << fixed << setprecision(2) << charge;
+}
