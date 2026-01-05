3af8d283beee7b4c95be49ab23441b88b6061210
변성훈 <sphz3813@gmail.com>
2026-01-05 21:38:13 +0900

    [Bronze IV] Title: :chino_shock:, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27310. ：chino＿shock：/：chino＿shock：.cc
@@ -0,0 +1,36 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+string emoji;
+int col, unb, len;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> emoji;
+
+	for (char ch : emoji) {
+		if (ch == '_') unb++;
+		else if (ch == ':') col++;
+		len++;
+	}
+
+	cout << len + col + unb * 5;
+}
