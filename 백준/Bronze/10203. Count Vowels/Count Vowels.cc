b6ed98deb3ee3590eccee1604e2bbb0183276ae7
변성훈 <sphz3813@gmail.com>
2026-01-12 19:54:30 +0900

    [Bronze IV] Title: Count Vowels, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10203. Count Vowels/Count Vowels.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int T;
+string S;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> T;
+
+	while (T--) {
+		cin >> S;
+
+		int cnt = 0;
+		for (char ch : S)
+			cnt += (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u');
+
+		cout << "The number of vowels in " << S << " is " << cnt << ".\n";
+	}
+}
