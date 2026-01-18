fdf8b7f426c146b08dafbed3f2278d8b8c7dbaa2
변성훈 <sphz3813@gmail.com>
2026-01-18 10:07:03 +0900

    [Silver I] Title: 소수를 분수로, Time: 0 ms, Memory: 2212 KB -BaekjoonHub

---
+++ 백준/Silver/5376. 소수를 분수로/소수를 분수로.cc
@@ -0,0 +1,65 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int T;
+string S;
+//=========================//
+
+ll gcd(ll a, ll b) {
+	return b == 0 ? a : gcd(b, a % b);
+}
+
+void no_cycle() {
+	ll ja = stoll(S.substr(2));
+	ll mo = pow(10, S.length() - 2);
+	ll g = gcd(ja, mo);
+	cout << ja / g << "/" << mo / g << '\n';
+}
+
+void has_cycle(int st) {
+	string tmp1 = S.substr(2, st - 2);
+	string tmp2 = S.substr(st + 1);
+	tmp2.pop_back(); // ')' 제거
+	ll ja = stoll(tmp1 + tmp2);
+
+	if (tmp1 != "")
+		ja -= stoll(tmp1);
+
+	ll x1 = pow(10, tmp1.length() + tmp2.length());
+	ll x2 = pow(10, st - 2);
+	ll mo = abs(x1 - x2);
+
+	ll g = gcd(ja, mo);
+	cout << ja / g << '/' << mo / g << '\n';
+}
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> T;
+
+	while (T--) {
+		cin >> S;
+		int st = S.find('(');
+		if (st == string::npos)
+			no_cycle();
+		else
+			has_cycle(st);
+	}
+}
