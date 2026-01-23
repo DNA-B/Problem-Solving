d9d3fd684ee15ac42652085fd03dae3365ae10bb
변성훈 <sphz3813@gmail.com>
2026-01-23 14:26:09 +0900

    [Gold II] Title: 합이 0인 네 정수, Time: 9508 ms, Memory: 166188 KB -BaekjoonHub

---
+++ 백준/Gold/7453. 합이 0인 네 정수/합이 0인 네 정수.cc
@@ -0,0 +1,49 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+int A[4005], B[4005], C[4005], D[4005];
+vector<int> AB_sum, CD_sum;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	for (int i = 0; i < N; i++)
+		cin >> A[i] >> B[i] >> C[i] >> D[i];
+
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			AB_sum.push_back(A[i] + B[j]);
+			CD_sum.push_back(C[i] + D[j]);
+		}
+	}
+
+	sort(CD_sum.begin(), CD_sum.end());
+
+	ll res = 0;
+	for (int x : AB_sum) {
+		res += (upper_bound(CD_sum.begin(), CD_sum.end(), -x)
+				- lower_bound(CD_sum.begin(), CD_sum.end(), -x));
+	}
+
+	cout << res;
+}
