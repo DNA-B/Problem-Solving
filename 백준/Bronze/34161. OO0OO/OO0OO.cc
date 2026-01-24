e07e35f458b2d415ad33a50f3ab0751066bbc4da
변성훈 <sphz3813@gmail.com>
2026-01-24 16:43:16 +0900

    [Bronze IV] Title: OO0OO, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/34161. OO0OO/OO0OO.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+string w, t, m;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	getline(cin, w);
+	getline(cin, t);
+	getline(cin, m);
+
+	for (int i = 0; i < 10000; i++)
+		cout << -1 << '\n';
+}
