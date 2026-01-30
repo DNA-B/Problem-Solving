e554861413355104a415ac5bd0e606a4e775f882
변성훈 <sphz3813@gmail.com>
2026-01-30 17:48:34 +0900

    [Silver III] Title: △, Time: 12 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/27966. △/△.cc
@@ -0,0 +1,31 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, len = 0;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+
+	cout << 1LL * (N - 1) * (N - 1) << "\n";
+	for (int i = 2; i <= N; i++)
+		cout << 1 << ' ' << i << '\n';
+}
