387ecc268f43dc32f8f67f5e6d1c1a287c257c2b
변성훈 <sphz3813@gmail.com>
2026-02-16 01:25:59 +0900

    [Bronze IV] Title: Malvika is peculiar about color of balloons, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/30156. Malvika is peculiar about color of balloons/Malvika is peculiar about color of balloons.cc
@@ -0,0 +1,35 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int T;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> T;
+
+	string S;
+	while (T--) {
+		cin >> S;
+		int A = count(S.begin(), S.end(), 'a');
+		int B = count(S.begin(), S.end(), 'b');
+		cout << min(A, B) << '\n';
+	}
+}
