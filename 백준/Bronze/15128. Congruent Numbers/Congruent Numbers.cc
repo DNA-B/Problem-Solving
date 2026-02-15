dd0a819ff382b6fc49bd6680078a2d2a8464f7f1
변성훈 <sphz3813@gmail.com>
2026-02-16 01:17:49 +0900

    [Bronze IV] Title: Congruent Numbers, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15128. Congruent Numbers/Congruent Numbers.cc
@@ -0,0 +1,32 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+ll p1, q1, p2, q2;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> p1 >> q1 >> p2 >> q2;
+
+	if ((p1 * p2) % (q1 * q2 * 2) == 0)
+		cout << 1;
+	else
+		cout << 0;
+}
