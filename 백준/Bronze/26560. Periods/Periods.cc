ed40de32393aa6ff2683ea9b340d665930ef2974
변성훈 <sphz3813@gmail.com>
2026-02-17 20:49:07 +0900

    [Bronze IV] Title: Periods, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/26560. Periods/Periods.cc
@@ -0,0 +1,37 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N;
+	cin.ignore();
+
+	string S;
+	while (N--) {
+		getline(cin, S);
+		if (S.back() != '.')
+			cout << S << ".\n";
+		else
+			cout << S << '\n';
+	}
+}
