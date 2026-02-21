5c5d83bd3c73dd6a6791ac2c166384154352cc30
변성훈 <sphz3813@gmail.com>
2026-02-22 01:45:04 +0900

    [Bronze III] Title: 앵그리 창영, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3034. 앵그리 창영/앵그리 창영.cc
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, H, W;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N >> W >> H;
+
+	int mx = (W * W) + (H * H);
+	int len;
+	for (int i = 0; i < N; i++) {
+		cin >> len;
+
+		if (len * len <= mx)
+			cout << "DA" << "\n";
+		else
+			cout << "NE" << "\n";
+	}
+}
