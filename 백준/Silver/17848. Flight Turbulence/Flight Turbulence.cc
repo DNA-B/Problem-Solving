19387a1a0b52240a47123d7de3ca1f5e1f0644be
변성훈 <sphz3813@gmail.com>
2026-02-21 16:39:02 +0900

    [Silver V] Title: Flight Turbulence, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/17848. Flight Turbulence/Flight Turbulence.cc
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int N, M;
+int seats[205];
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> N >> M;
+
+	for (int i = 1; i <= N; i++)
+		cin >> seats[i];
+
+	int cnt = 0;
+	if (seats[M] != M) {
+		int end = M;
+		while (seats[M] != end) {
+			cnt++;
+			M = seats[M];
+		}
+		cnt++;
+	}
+
+	cout << cnt;
+}
