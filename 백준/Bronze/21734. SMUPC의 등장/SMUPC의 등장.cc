6107b9e9f147ac4fe80239912de6f3ae0eaa37b7
변성훈 <sphz3813@gmail.com>
2026-02-22 09:32:26 +0900

    [Bronze II] Title: SMUPC의 등장, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/21734. SMUPC의 등장/SMUPC의 등장.cc
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+string S;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> S;
+
+	for (char ch : S) {
+		int tmp = ch;
+		string chToInt = to_string(tmp);
+
+		int rep = 0;
+		for (char n : chToInt)
+			rep += n - '0';
+
+		while (rep--)
+			cout << ch;
+		cout << '\n';
+	}
+}
