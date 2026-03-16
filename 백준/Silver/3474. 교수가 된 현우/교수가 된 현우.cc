f13d48e20540b574128103eb7967431740c76d61
변성훈 <sphz3813@gmail.com>
2026-03-16 21:27:14 +0900

    [Silver III] Title: 교수가 된 현우, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/3474. 교수가 된 현우/교수가 된 현우.cc
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include<bits/stdc++.h>
+
+using namespace std;
+
+//========= MACRO =========//
+#define PI 3.1415926535897932
+#define INF 0x3f3f3f3f
+#define LL_INF 1e18
+#define ll long long
+#define X first
+#define Y second
+//=========================//
+
+//======= VARIABLEs =======//
+int T;
+//=========================//
+
+int main() {
+	cin.tie(nullptr)->ios_base::sync_with_stdio(false);
+#ifdef _DEBUG
+	freopen("input.txt", "r", stdin);
+	freopen("output.txt", "w", stdout);
+#endif // _DEBUG
+
+	cin >> T;
+
+	while (T--) {
+		int N;
+		cin >> N;
+
+		ll res = 0;
+		while (N >= 5) {
+			N /= 5;
+			res += N;
+		}
+
+		cout << res << '\n';
+	}
+}
